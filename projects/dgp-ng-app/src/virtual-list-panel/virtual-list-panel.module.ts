aed38779f377af030f9b00061ce2a1e65d802b6e
dgp-web <dgp-web@outlook.de>
2019-12-12 09:27:58 +0100

    Remove unneeded empty-state module from vlist-panel

--- projects/dgp-ng-app/src/virtual-list-panel/virtual-list-panel.module.ts
+++ projects/dgp-ng-app/src/virtual-list-panel/virtual-list-panel.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ScrollingModule } from "@angular/cdk/scrolling";
-import { DgpEmptyStateModule } from "../empty-state";
 import * as components from "./components";
 import * as directives from "./directives";
 
 @NgModule({
     imports: [
         CommonModule,
-        DgpEmptyStateModule,
         ScrollingModule
     ],
     declarations: [
