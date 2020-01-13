7ca712039130f940812499e65f5e8f148bd6e38f
dgp-web <dgp-web@outlook.de>
2020-01-13 06:26:22 +0100

    Add spacer docs module

---
+++ projects/ng-app/src/features/spacer-docs/containers/spacer-docs-page.component.ts
@@ -0,0 +1,68 @@
+import { Component, ChangeDetectionStrategy } from "@angular/core";
+
+@Component({
+    selector: "dgp-spacer-docs-page",
+    template: `
+        <dgp-page-header>
+            <dgp-hamburger-menu-toggle></dgp-hamburger-menu-toggle>
+            Spacer
+        </dgp-page-header>
+
+        <dgp-docs-page>
+            <dgp-docs-page-content>
+
+                <dgp-docs-chapter-title>Spacer</dgp-docs-chapter-title>
+
+                <p>
+                    Invisible component that fills empty space.
+                </p>
+
+                <dgp-docs-section-title>
+                    1: Import DgSpacerModule in your feature module.
+                </dgp-docs-section-title>
+
+                <dgp-docs-code-block [code]="moduleImportCode"
+                                     language="typescript"></dgp-docs-code-block>
+
+                <dgp-docs-section-title>
+                    2: Add the component to your template.
+                </dgp-docs-section-title>
+
+                <dgp-docs-code-block [code]="templateCode"
+                                     language="html"></dgp-docs-code-block>
+
+            </dgp-docs-page-content>
+        </dgp-docs-page>
+    `,
+    styles: [`
+
+    `],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SpacerDocPageComponent {
+
+    readonly moduleImportCode = `
+@NgModule({
+    imports: [
+        DgpSpacerModule,
+        // ...
+    ]
+})
+export class FeatureModule {}
+`;
+
+    readonly templateCode = `
+    <mat-toolbar>
+
+    Feature title
+
+    <dgp-spacer></dgp-spacer>
+
+    <button mat-icon-button>
+        <mat-icon>add</mat-icon>
+    </button>
+
+</mat-toolbar>
+`;
+
+}
---
+++ projects/ng-app/src/features/spacer-docs/spacer-docs.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { SpacerDocPageComponent } from "./containers";
+import { DocsPageModule } from "../shared";
+import { RouterModule } from "@angular/router";
+
+@NgModule({
+    imports: [
+        RouterModule.forChild([{
+            path: "spacer",
+            component: SpacerDocPageComponent
+        }]),
+        DocsPageModule
+    ],
+    declarations: [
+        SpacerDocPageComponent
+    ]
+})
+export class SpacerDocsModule {
+}
