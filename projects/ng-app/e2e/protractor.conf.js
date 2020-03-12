10a9e24ac01923cde05ee37c72634df25604bb5e
dgp-web <dgp-web@outlook.de>
2020-03-12 10:22:37 +0100

    Switch to chrome-headless for testing

--- projects/ng-app/e2e/protractor.conf.js
+++ projects/ng-app/e2e/protractor.conf.js
@@ -14,6 +14,9 @@ exports.config = {
     ],
     capabilities: {
         browserName: "chrome",
+        chromeOptions: {
+            "args": ["--headless", "--window-size=1920x1080"]
+        }
     },
     directConnect: true,
     baseUrl: "http://localhost:4200/",
