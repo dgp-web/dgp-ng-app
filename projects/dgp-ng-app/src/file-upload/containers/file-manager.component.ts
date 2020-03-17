96099e67f62e0c86733e8542918a12f9c0a36472
dgp-web <dgp-web@outlook.de>
2020-03-17 16:10:04 +0100

    Adjust drop handling

--- projects/dgp-ng-app/src/file-upload/containers/file-manager.component.ts
+++ projects/dgp-ng-app/src/file-upload/containers/file-manager.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component,  } from "@angular/core";
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnDestroy, } from "@angular/core";
 
 @Component({
     selector: "dgp-file-manager",
@@ -14,9 +14,39 @@ import { ChangeDetectionStrategy, Component,  } from "@angular/core";
             width: 100%;
             height: 100%;
         }
-   `],
+    `],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FileManagerComponent {
+export class FileManagerComponent implements AfterViewInit, OnDestroy {
+
+    readonly dragOverHandler = (e) => {
+        console.log("File(s) in drop zone");
+        console.log(e);
+        // Prevent default behavior (Prevent file from being opened)
+        e.preventDefault();
+    };
+
+    readonly dropHandler = (e) => {
+        console.log("File(s) dropped");
+        console.log(e);
+        // Prevent default behavior (Prevent file from being opened)
+        e.preventDefault();
+    };
+
+    constructor(
+        private readonly elementRef: ElementRef
+    ) {
+    }
+
+
+    ngAfterViewInit(): void {
+        document.querySelector(".cdk-overlay-backdrop").addEventListener("dragover", this.dragOverHandler);
+        document.querySelector(".cdk-overlay-backdrop").addEventListener("drop", this.dropHandler);
+    }
+
+    ngOnDestroy(): void {
+        document.querySelector(".cdk-overlay-backdrop").removeEventListener("dragover", this.dragOverHandler);
+        document.querySelector(".cdk-overlay-backdrop").removeEventListener("drop", this.dropHandler);
+    }
 
 }
