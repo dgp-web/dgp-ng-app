134c4387a60eb121896fecc1e3e755c28fe582a1
dgp-web <dgp-web@outlook.de>
2020-03-26 09:32:55 +0100

    Adjust getFileFromFileItem

--- projects/dgp-ng-app/src/file-upload/functions.ts
+++ projects/dgp-ng-app/src/file-upload/functions.ts
@@ -52,16 +52,20 @@ export function getFileItemsFromFileList(fileList: FileList): ReadonlyArray<File
 
 export function getFileFromFileItem$(fileItem: FileItem): Promise<File> {
 
-    return new Promise<File>((resolve, reject) => {
+    return new Promise<File>(resolve => {
 
         return fetch(fileItem.url, {
             credentials: "include"
         }).then(x => x.blob()).then(x => {
-            const file = new File([x], fileItem.fileName + "." + fileItem.extension, {
+            const file = new Blob([x], {
                 type: getMimeTypeFromExtension(fileItem.extension)
             });
 
-            resolve(file);
+            (file as any).name = fileItem.fileName + "." + fileItem.extension;
+
+            resolve(file as File);
+        }).catch(reason => {
+            console.error(reason);
         });
 
     });
