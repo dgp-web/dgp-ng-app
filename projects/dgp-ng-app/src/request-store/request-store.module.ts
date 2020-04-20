70f042dd17ee1f1a375f587d34a4936da9e2b0e4
dgp <dgp-web@outlook.de>
2020-04-20 20:14:29 +0200

    Refactor and test reducers of request store

--- projects/dgp-ng-app/src/request-store/request-store.module.ts
+++ projects/dgp-ng-app/src/request-store/request-store.module.ts
@@ -1,22 +1,32 @@
-import { NgModule } from "@angular/core";
+import { FactoryProvider, InjectionToken, NgModule } from "@angular/core";
 import { EffectsModule } from "@ngrx/effects";
 import { StoreModule } from "@ngrx/store";
-import { requestStoreFeature } from "./models";
-import { requestStoreReducer, requestStoreReducerProviders } from "./reducers/reducer";
+import { requestStoreFeature, RequestStoreState } from "./models";
 import { RequestEffects } from "./effects";
+import { requestStoreReducer } from "./reducers";
+
+export const REQUEST_STORE_REDUCER = new InjectionToken<RequestStoreState>("RequestStoreReducer");
+
+export function requestStoreReducerFactory() {
+    return requestStoreReducer;
+}
+
+export const requestStoreReducerProvider: FactoryProvider = {
+    provide: REQUEST_STORE_REDUCER,
+    useFactory: requestStoreReducerFactory
+};
+
 
 @NgModule({
     imports: [
-        StoreModule.forFeature(requestStoreFeature, requestStoreReducer),
+        StoreModule.forFeature(requestStoreFeature, REQUEST_STORE_REDUCER),
 
         EffectsModule.forFeature([
             RequestEffects
         ])
     ],
     providers: [
-        requestStoreReducerProviders,
-
-        RequestEffects
+        requestStoreReducerProvider
     ]
 })
 export class DgpRequestStoreModule {
