f160ef53eb7b8509a1694d87a21abc856c9a8946
dgp <dgp-web@outlook.de>
2020-04-25 16:26:54 +0200

    Add effects.spec for hamburger-shell

---
+++ projects/dgp-ng-app/src/hamburger-shell/effects.spec.ts
@@ -0,0 +1,23 @@
+import { HamburgerShellEffects } from "./effects";
+import { TestBed } from "@angular/core/testing";
+
+describe(HamburgerShellEffects.name, () => {
+
+
+
+    beforeEach(() => {
+
+        const testBed = TestBed.configureTestingModule({
+            imports: [],
+            providers: [
+                HamburgerShellEffects
+            ]
+        });
+
+    });
+
+    it(`should create`, () => {
+
+    });
+
+});
