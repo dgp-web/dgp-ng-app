971d8d2d2bfb6f281e200455897d7d128631f65d
dgp <dgp-web@outlook.de>
2020-04-26 17:17:04 +0200

    Cleanup public api

--- projects/dgp-ng-app/src/public-api.ts
+++ projects/dgp-ng-app/src/public-api.ts
@@ -3,26 +3,26 @@
  */
 
 // authentication
-export * from "./authentication/actions";
-export * from "./authentication/api-clients/authentication.api-client";
-export * from "./authentication/guards/authentication.guard";
-export * from "./authentication/models";
-export * from "./authentication/selectors";
+export { authenticateUser } from "./authentication/actions";
+export { AuthenticationApiClient, AuthenticationApiClientProvider } from "./authentication/api-clients/authentication.api-client";
+export { AuthenticationGuard } from "./authentication/guards/authentication.guard";
+export { AuthenticationState, authenticationStoreFeature } from "./authentication/models";
+export { getAuthenticatedUserSelector, getIsAuthenticatedSelector } from "./authentication/selectors";
 export * from "./authentication/services/authentication.service";
-export * from "./authentication/services/initialization.service";
-export * from "./authentication/authentication.module";
+export { InitializationService, InitializationServiceProvider } from "./authentication/services/initialization.service";
+export { DgpAuthenticationModule, AuthenticationModuleSettings } from "./authentication/authentication.module";
 
 // broadcast
-export * from "./broadcast/actions";
-export * from "./broadcast/guards/no-peon.guard";
-export * from "./broadcast/functions/create-guid.function";
+export { setBroadcastChannelDataId, SetBroadcastChannelDataIdAction, SetOwnBroadcastRoleAction } from "./broadcast/actions";
+export { NoPeonGuard } from "./broadcast/guards/no-peon.guard";
+export { createGuid } from "./broadcast/functions/create-guid.function";
 export * from "./broadcast/models";
-export * from "./broadcast/store";
-export * from "./broadcast/broadcast-store.module";
+export { BroadcastState, broadcastStoreFeature, broadcastStoreFeatureSelector, getOwnBroadcastRoleSelector } from "./broadcast/store";
+export { DgpBroadcastStoreModule } from "./broadcast/broadcast-store.module";
 
 // empty-state
-export * from "./empty-state/components/empty-state.component";
-export * from "./empty-state/empty-state.module";
+export { EmptyStateComponent } from "./empty-state/components/empty-state.component";
+export { DgpEmptyStateModule } from "./empty-state/empty-state.module";
 
 // file-upload
 export * from "./file-viewer/models";
@@ -44,8 +44,8 @@ export * from "./file-upload/directive/drag-file-listener.directive";
 export * from "./file-upload/directive/open-file-manager-via-short-key.directive";
 export * from "./file-upload/functions";
 export * from "./file-upload/models";
-export * from "./file-upload/store";
-export * from "./file-upload/file-upload.module";
+export { fileUploadEntityStore } from "./file-upload/store";
+export { DgpFileUploadModule } from "./file-upload/file-upload.module";
 
 // hamburger-shell
 export * from "./hamburger-shell/actions";
@@ -58,61 +58,69 @@ export * from "./hamburger-shell/components/list-details-page/list-details-page.
 export * from "./hamburger-shell/components/page-header/page-header.component";
 export * from "./hamburger-shell/components/page-header/page-header.module";
 export * from "./hamburger-shell/models";
-export * from "./hamburger-shell/selectors";
-export * from "./hamburger-shell/hamburger-shell.module";
+export {
+    hamburgerMenuModeSelector, hamburgerShellFeatureSelector, isHamburgerMenuOpenSelector, isPageMenuOpenSelector, pageMenuModeSelector
+} from "./hamburger-shell/selectors";
+export { DgpHamburgerShellModule } from "./hamburger-shell/hamburger-shell.module";
 
 // hmr
-export * from "./hmr/hmr.actions";
-export * from "./hmr/hmr.reducer";
-export * from "./hmr/hmr-app";
+export { hotReload } from "./hmr/hmr.actions";
+export { hmrReducer } from "./hmr/hmr.reducer";
+export { DgpNgApp } from "./hmr/hmr-app";
 
 // log
-export * from "./log/actions";
-export * from "./log/models";
-export * from "./log/models";
-export * from "./log/log.module";
+export { addLogEntry, logError, LogErrorAction, logErrorActionType } from "./log/actions";
+export { LogEntry, Severity } from "./log/models";
+export { LogState, logStoreFeature } from "./log/models";
+export { DgpLogModule } from "./log/log.module";
 
 // request-store
-export * from "./request-store/models";
-export * from "./request-store/models";
-export * from "./request-store/actions";
-export * from "./request-store/selectors";
-export * from "./request-store/request-store.module";
+export { requestStoreFeature } from "./request-store/models";
+export { RequestState } from "./request-store/models";
+export {
+    ScheduleRequestAction, scheduleRequest, scheduleRequestActionType
+} from "./request-store/actions";
+export {
+    hasPendingRequestsSelector, hasPendingRequests, requestStateSelector
+} from "./request-store/selectors";
+export { DgpRequestStoreModule } from "./request-store/request-store.module";
 
 // routing-overlay
-export * from "./routing-overlay/actions";
-export * from "./routing-overlay/routing-overlay.module";
+export { showLoadingSpinner } from "./routing-overlay/actions";
+export { DgpRoutingOverlayModule } from "./routing-overlay/routing-overlay.module";
 
 // safe pipe
-export * from "./safe/safe.pipe";
-export * from "./safe/safe-pipe.module";
+export { SafePipe } from "./safe/safe.pipe";
+export { SafePipeModule } from "./safe/safe-pipe.module";
 
 // spacer
-export * from "./spacer/components/spacer.component";
-export * from "./spacer/spacer.module";
+export { SpacerComponent } from "./spacer/components/spacer.component";
+export { DgpSpacerModule } from "./spacer/spacer.module";
 
 // table-cell
-export * from "./table-cell-editor/components/table-cell.component";
-export * from "./table-cell-editor/directives/table-cell-editor.directive";
-export * from "./table-cell-editor/table-cell.module";
+export { DgpTableCellComponent } from "./table-cell-editor/components/table-cell.component";
+export { DgpTableCelLEditorDirective } from "./table-cell-editor/directives/table-cell-editor.directive";
+export { DgpTableCellModule } from "./table-cell-editor/table-cell.module";
 
 // theme-switcher
-export * from "./theme-switcher/actions";
-export * from "./theme-switcher/components/dark-mode-toggle.component";
-export * from "./theme-switcher/directives/theme-host.directive";
-export * from "./theme-switcher/models";
-export * from "./theme-switcher/selectors";
-export * from "./theme-switcher/theme-switcher.module";
+export { setIsDarkModeActive, toggleDarkMode } from "./theme-switcher/actions";
+export { DarkModeToggleComponent } from "./theme-switcher/components/dark-mode-toggle.component";
+export { ThemeHostDirective } from "./theme-switcher/directives/theme-host.directive";
+export { ThemeSwitcherConfig, ThemeSwitcherState, themeSwitcherStoreFeature } from "./theme-switcher/models";
+export {
+    themeSwitcherFeatureSelector, isDarkModeActiveSelector, isDarkModeActive
+} from "./theme-switcher/selectors";
+export { DgpThemeSwitcherModule } from "./theme-switcher/theme-switcher.module";
 
 // utils
-export * from "./utils/container.component-base";
-export * from "./utils/model-editor.component-base";
-export * from "./utils/select-entity-via-route.resolver-base";
+export { DgpContainer } from "./utils/container.component-base";
+export { DgpModelEditorComponentBase } from "./utils/model-editor.component-base";
+export { DgpSelectEntityViaRouteResolver, SelectEntityViaRouteResolverConfig } from "./utils/select-entity-via-route.resolver-base";
 
 // virtual-list panel
-export * from "./virtual-list-panel/directives/virtual-list-item.directive";
-export * from "./virtual-list-panel/components/virtual-list-panel.component";
-export * from "./virtual-list-panel/virtual-list-panel.module";
+export { VirtualListItemDirective } from "./virtual-list-panel/directives/virtual-list-item.directive";
+export { VirtualListPanelComponent } from "./virtual-list-panel/components/virtual-list-panel.component";
+export { DgpVirtualListPanelModule } from "./virtual-list-panel/virtual-list-panel.module";
 
 export * from "./dgp-ng-app.module";
 
