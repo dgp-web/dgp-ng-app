544f386f22d77d28025a975a69c8fccbe0602778
dgp <dgp-web@outlook.de>
2020-05-14 07:03:41 +0200

    Comment in split panel

--- projects/dgp-labs/src/app/app.component.ts
+++ projects/dgp-labs/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: "app-root",
     template: `
-        <dgp-docking-layout dgpThemeHost>
+        <!--<dgp-docking-layout dgpThemeHost>
 
             <dgp-docking-layout-item type="column">
 
@@ -52,9 +52,9 @@ import { Component } from "@angular/core";
                 </dgp-docking-layout-item>
 
             </dgp-docking-layout-item>
-        </dgp-docking-layout>
+        </dgp-docking-layout>-->
 
-        <!--<dgp-split-panel orientation="horizontal"
+        <dgp-split-panel orientation="horizontal"
                          dgpThemeHost>
             <dgp-split-panel-content size="20">
                 <ng-template>
@@ -88,7 +88,7 @@ import { Component } from "@angular/core";
 
                 </ng-template>
             </dgp-split-panel-content>
-        </dgp-split-panel>-->
+        </dgp-split-panel>
     `,
     styleUrls: ["./app.component.scss"]
 })
