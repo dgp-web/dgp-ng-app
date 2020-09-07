c1c4de81ad026ead159d106215a1f51bef63b02d
dgp <dgp-web@outlook.de>
2020-09-07 14:20:05 +0200

    Remove outcommented rant

--- projects/dgp-labs/src/test.ts
+++ projects/dgp-labs/src/test.ts
@@ -1,19 +1,5 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-/*
-require("core-js");
-
-require("zone.js/dist/zone");
-require("zone.js/dist/long-stack-trace-zone");
-require("zone.js/dist/proxy");
-require("zone.js/dist/sync-test");
-require("zone.js/dist/jasmine-patch");
-require("zone.js/dist/async-test");
-require("zone.js/dist/fake-async-test");
-
-require("rxjs");
-*/
-
 import "core-js";
 import "zone.js/dist/zone";
 import "zone.js/dist/zone-testing";
