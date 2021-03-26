b2a6a387ef5a8d1e1f30f9b7e5591fc6ab881ba9
dgp <dgp-web@outlook.de>
2021-03-26 07:47:10 +0100

    11.1.4 Make chart container focusable
    We remove all javascript and work with css only

--- projects/dgp-ng-charts/src/lib/chart-container/components/chart-container.component.ts
+++ projects/dgp-ng-charts/src/lib/chart-container/components/chart-container.component.ts
@@ -1,24 +1,17 @@
-import { ChangeDetectionStrategy, Component, ElementRef, HostListener } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
-import { debounceTime } from "rxjs/operators";
+import { ChangeDetectionStrategy, Component, ElementRef, HostBinding } from "@angular/core";
 
 @Component({
     selector: "dgp-chart-container",
     template: `
-
         <ng-content></ng-content>
 
-        <mat-card *ngIf="areControlsVisible$ | async"
-                  class="controls"
+        <mat-card class="controls"
                   [style.left.px]="getControlsLeft()"
-                  [style.top.px]="getControlsTop()"
-                  (mouseover)="showControls()"
-                  (mouseout)="hideControls()">
+                  [style.top.px]="getControlsTop()">
 
             <ng-content select="[chart-actions]"></ng-content>
 
         </mat-card>
-
     `,
     styles: [`
         :host {
@@ -27,26 +20,30 @@ import { debounceTime } from "rxjs/operators";
             position: relative;
         }
 
+        :host:hover .controls, :host:focus-within .controls {
+            display: flex;
+        }
+
         .controls {
             z-index: 100;
             position: fixed;
-            display: flex;
+            display: none;
             flex-direction: column;
             padding: 4px !important;
         }
+
     `],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChartContainerComponent {
 
-    readonly areControlsVisibleScheduler$ = new BehaviorSubject<boolean>(false);
-    readonly areControlsVisible$ = this.areControlsVisibleScheduler$.pipe(debounceTime(250));
+    @HostBinding("tabindex")
+    readonly tabindex = 0;
 
     constructor(
         readonly elRef: ElementRef
     ) {
     }
-
 
     getControlsLeft(): number {
         return this.elRef.nativeElement.getBoundingClientRect().right - 4;
@@ -54,16 +51,6 @@ export class ChartContainerComponent {
 
     getControlsTop(): number {
         return this.elRef.nativeElement.getBoundingClientRect().top;
-    }
-
-    @HostListener("mouseover")
-    showControls() {
-        this.areControlsVisibleScheduler$.next(true);
-    }
-
-    @HostListener("mouseout")
-    hideControls() {
-        this.areControlsVisibleScheduler$.next(false);
     }
 
 }
