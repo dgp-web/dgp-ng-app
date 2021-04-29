f049bc5c13434c5fe0820679b2744654bd1026a2
dgp <dgp-web@outlook.de>
2021-04-29 08:59:16 +0200

    Extend action context so it can be configured with either a value or a key

--- projects/dgp-ng-app/src/action-context/selectors/is-any-action-context-selected.selector.ts
+++ projects/dgp-ng-app/src/action-context/selectors/is-any-action-context-selected.selector.ts
@@ -2,6 +2,6 @@ import { createSelector } from "@ngrx/store";
 import { actionContextFeatureSelector } from "./action-context-feature.selector";
 import { notNullOrUndefined } from "../../utils/null-checking.functions";
 
-export const isActionContextSelectedSelector = createSelector(
+export const isActionContextSelected = createSelector(
     actionContextFeatureSelector, x => notNullOrUndefined(x.selectedActionContextKey)
 );
