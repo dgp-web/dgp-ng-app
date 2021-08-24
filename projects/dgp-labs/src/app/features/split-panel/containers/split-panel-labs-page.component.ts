17a0768f8db7bd49a80596a75d5fa72c8a2e2c92
dgp <dgp-web@outlook.de>
2021-08-24 16:35:49 +0200

    Adjust demo case

--- projects/dgp-labs/src/app/features/split-panel/containers/split-panel-labs-page.component.ts
+++ projects/dgp-labs/src/app/features/split-panel/containers/split-panel-labs-page.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from "@angular/cdk/drag-drop";
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
@@ -30,13 +31,35 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
                     <dgp-split-panel orientation="vertical">
                         <dgp-split-panel-content size="50">
                             <ng-template>
-                                C
+                                <div class="list-01 dgp-drag-list"
+                                     cdkDropList
+                                     cdkDropListOrientation="horizontal"
+                                     id="sourceList"
+                                     #sourceList="cdkDropList"
+                                     [cdkDropListData]="itemsFromList01"
+                                     [cdkDropListConnectedTo]="['targetList']"
+                                     (cdkDropListDropped)="drop($event)">
+                                    <div *ngFor="let item of itemsFromList01"
+                                         cdkDrag
+                                         class="item dgp-drag-item">{{ item.title }}</div>
+                                </div>
                             </ng-template>
                         </dgp-split-panel-content>
 
                         <dgp-split-panel-content size="50">
                             <ng-template>
-                                D
+                                <div class="list-02 dgp-drag-list"
+                                     cdkDropList
+                                     cdkDropListOrientation="horizontal"
+                                     #targetList="cdkDropList"
+                                     id="targetList"
+                                     [cdkDropListData]="itemsFromList02"
+                                     [cdkDropListConnectedTo]="['sourceList']"
+                                     (cdkDropListDropped)="drop($event)">
+                                    <div *ngFor="let item of itemsFromList02"
+                                         cdkDrag
+                                         class="item dgp-drag-item">{{ item.title }}</div>
+                                </div>
                             </ng-template>
                         </dgp-split-panel-content>
                     </dgp-split-panel>
@@ -60,9 +83,55 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
             flex-grow: 1;
             overflow: auto;
         }
+
+        .item {
+            width: 100px;
+            height: 100px;
+            margin: 8px;
+            border: 1px solid gray;
+            cursor: move;
+        }
+
+        .list-01 {
+            display: flex;
+            flex-wrap: wrap;
+            overflow: auto;
+        }
+
+        .list-02 {
+            display: flex;
+            flex-wrap: wrap;
+            overflow: auto;
+        }
+
+
     `],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SplitPanelLabsPageComponent {
 
+    itemsFromList01 = [
+        {title: "A"},
+        {title: "B"},
+        {title: "C"},
+        {title: "D"},
+    ];
+
+    itemsFromList02 = [
+        {title: "E"},
+        {title: "F"},
+        {title: "G"},
+        {title: "H"},
+    ];
+
+    drop(event: CdkDragDrop<{ title: string; }[]>) {
+        if (event.previousContainer === event.container) {
+            moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+        } else {
+            transferArrayItem(event.previousContainer.data,
+                event.container.data,
+                event.previousIndex,
+                event.currentIndex);
+        }
+    }
 }
--- projects/dgp-labs/src/app/features/split-panel/split-panel-labs.module.ts
+++ projects/dgp-labs/src/app/features/split-panel/split-panel-labs.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import { SplitPanelLabsPageComponent } from "./containers/split-panel-labs-page.component";
 import { DgpSplitPanelModule } from "dgp-ng-docking-layout";
 import { DgpHamburgerMenuToggleModule, DgpPageHeaderModule, DgpThemeSwitcherModule } from "dgp-ng-app";
 import { RouterModule } from "@angular/router";
+import { DragDropModule } from "@angular/cdk/drag-drop";
 
 @NgModule({
     imports: [
+        CommonModule,
         DgpSplitPanelModule,
         DgpThemeSwitcherModule,
         RouterModule.forRoot([{
-        path: "split-panel",
-        component: SplitPanelLabsPageComponent
-    }], { relativeLinkResolution: 'legacy' }),
+            path: "split-panel",
+            component: SplitPanelLabsPageComponent
+        }], {relativeLinkResolution: 'legacy'}),
         DgpHamburgerMenuToggleModule,
-        DgpPageHeaderModule
+        DgpPageHeaderModule,
+        DragDropModule
     ],
     declarations: [
         SplitPanelLabsPageComponent
