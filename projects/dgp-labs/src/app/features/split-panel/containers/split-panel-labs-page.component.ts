959e6a1c8e2b48659a62d5c3e32bff01eb5fb969
dgp <dgp-web@outlook.de>
2021-08-24 16:59:31 +0200

    Adjust demo

--- projects/dgp-labs/src/app/features/split-panel/containers/split-panel-labs-page.component.ts
+++ projects/dgp-labs/src/app/features/split-panel/containers/split-panel-labs-page.component.ts
@@ -1,4 +1,4 @@
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from "@angular/cdk/drag-drop";
+import { CdkDragDrop } from "@angular/cdk/drag-drop";
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
@@ -31,34 +31,40 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
                     <dgp-split-panel orientation="vertical">
                         <dgp-split-panel-content size="50">
                             <ng-template>
-                                <div class="list-01 dgp-drag-list"
-                                     cdkDropList
-                                     cdkDropListOrientation="horizontal"
-                                     id="sourceList"
-                                     #sourceList="cdkDropList"
-                                     [cdkDropListData]="itemsFromList01"
-                                     [cdkDropListConnectedTo]="['targetList']"
-                                     (cdkDropListDropped)="drop($event)">
-                                    <div *ngFor="let item of itemsFromList01"
-                                         cdkDrag
-                                         class="item dgp-drag-item">{{ item.title }}</div>
+                                <div class="list-01"
+                                     cdkDropListGroup>
+                                    <div *ngFor="let item of itemsFromList01; let i=index;"
+                                         cdkDropList
+                                         cdkDropListOrientation="horizontal"
+                                         class="item"
+                                         [cdkDropListData]="{item:item,index:i}"
+                                         (cdkDropListDropped)="drop($event)">
+                                        <div cdkDrag
+                                            class="item-content">
+                                            <div class="item-content drag-placeholder" *cdkDragPlaceholder>Test</div>
+                                            {{ item.title }}
+                                        </div>
+                                    </div>
                                 </div>
                             </ng-template>
                         </dgp-split-panel-content>
 
                         <dgp-split-panel-content size="50">
                             <ng-template>
-                                <div class="list-02 dgp-drag-list"
-                                     cdkDropList
-                                     cdkDropListOrientation="horizontal"
-                                     #targetList="cdkDropList"
-                                     id="targetList"
-                                     [cdkDropListData]="itemsFromList02"
-                                     [cdkDropListConnectedTo]="['sourceList']"
-                                     (cdkDropListDropped)="drop($event)">
-                                    <div *ngFor="let item of itemsFromList02"
-                                         cdkDrag
-                                         class="item dgp-drag-item">{{ item.title }}</div>
+                                <div class="list-02"
+                                     cdkDropListGroup>
+                                    <div *ngFor="let item of itemsFromList02; let i=index;"
+                                         cdkDropList
+                                         cdkDropListOrientation="horizontal"
+                                         class="item"
+                                         [cdkDropListData]="{item:item,index:i}"
+                                         (cdkDropListDropped)="drop($event)">
+                                        <div cdkDrag
+                                             class="item-content">
+                                            <div class="item-content drag-placeholder" *cdkDragPlaceholder>Test</div>
+                                            {{ item.title }}
+                                        </div>
+                                    </div>
                                 </div>
                             </ng-template>
                         </dgp-split-panel-content>
@@ -90,20 +96,35 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
             margin: 8px;
             border: 1px solid gray;
             cursor: move;
+            position:relative;
+            overflow: hidden;
+        }
+
+        .item-content {
+            width: 100%;
+            height: 100%;
         }
 
         .list-01 {
             display: flex;
             flex-wrap: wrap;
             overflow: auto;
+            height: 100%;
         }
 
         .list-02 {
             display: flex;
             flex-wrap: wrap;
             overflow: auto;
+            height: 100%;
         }
 
+        .drag-placeholder {
+            background: #ccc;
+            border: dotted 3px #999;
+            min-height: 60px;
+            transition: transform 250ms cubic-bezier(0, 0, 0.2, 1);
+        }
 
     `],
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -124,14 +145,16 @@ export class SplitPanelLabsPageComponent {
         {title: "H"},
     ];
 
-    drop(event: CdkDragDrop<{ title: string; }[]>) {
-        if (event.previousContainer === event.container) {
-            moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-        } else {
-            transferArrayItem(event.previousContainer.data,
-                event.container.data,
-                event.previousIndex,
-                event.currentIndex);
-        }
+    drop(event: CdkDragDrop<{ item: { title: string; }; index: number; }>) {
+        this.itemsFromList01[event.previousContainer.data.index] = event.container.data.item;
+        this.itemsFromList01[event.container.data.index] = event.previousContainer.data.item;
+        /* if (event.previousContainer === event.container) {
+             moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+         } else {
+             transferArrayItem(event.previousContainer.data,
+                 event.container.data,
+                 event.previousIndex,
+                 event.currentIndex);
+         }*/
     }
 }
