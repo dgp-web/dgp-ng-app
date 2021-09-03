9d33504d981cd4f7765efeed49da8070b7852460
dgp <dgp-web@outlook.de>
2021-09-03 12:52:22 +0200

    HARD RESET

--- projects/dgp-ng-app/src/authentication/actions.ts
+++ projects/dgp-ng-app/src/authentication/actions.ts
@@ -1,7 +1,7 @@
 import { createAction, props } from "@ngrx/store";
 
 export const authenticationActionTypePrefix = "[Authentication] ";
-
+ 
 export const authenticateUser = createAction(authenticationActionTypePrefix + "AuthenticateUser", props<{ readonly user: any; }>());
 export const cacheInitialUrl = createAction(authenticationActionTypePrefix + "CacheInitialUrl", props<{ readonly initialUrl: string }>());
 export const registerAuthenticateError = createAction(authenticationActionTypePrefix + "RegisterAuthenticationError", props<{ readonly error: any }>());
