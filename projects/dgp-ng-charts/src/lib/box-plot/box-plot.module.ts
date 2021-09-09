f4ef028e8306efca63f52012d51b234a3fb945e7
dgp <dgp-web@outlook.de>
2021-09-09 14:57:08 +0200

    Adjust directives

--- projects/dgp-ng-charts/src/lib/box-plot/box-plot.module.ts
+++ projects/dgp-ng-charts/src/lib/box-plot/box-plot.module.ts
@@ -5,18 +5,9 @@ import { MatTooltipModule } from "@angular/material/tooltip";
 import { MatIconModule } from "@angular/material/icon";
 import { MatDialogModule } from "@angular/material/dialog";
 import { DgpChartContainerModule } from "../chart-container/chart-container.module";
-import { BoxPlotBottomAxisDirective } from "./directives/box-plot-bottom-axis.directive";
-import { BoxPlotLeftAxisDirective } from "./directives/box-plot-left-axis.directive";
-import { BoxPlotWhiskerDirective } from "./directives/box-plot-whisker.directive";
-import { BoxPlotUpperAntennaDirective } from "./directives/box-plot-upper-antenna.directive";
-import { BoxPlotLowerAntennaDirective } from "./directives/box-plot-lower-antenna.directive";
-import { BoxPlotBoxDirective } from "./directives/box-plot-box.directive";
-import { BoxPlotMedianDirective } from "./directives/box-plot-median.directive";
-import { BoxPlotOutlierDirective } from "./directives/box-plot-outlier.directive";
-import { BoxPlotBrushSelectorDirective } from "./directives/box-plot-brush-selector.directive";
-import { BoxPlotOutlierTooltipDirective } from "./directives/box-plot-outlier-tooltip.directive";
 import { MatMenuModule } from "@angular/material/menu";
 import { components } from "./components/components";
+import { directives } from "./directives/directives";
 
 @NgModule({
     imports: [
@@ -29,30 +20,12 @@ import { components } from "./components/components";
         MatMenuModule
     ],
     declarations: [
-        BoxPlotBottomAxisDirective,
-        BoxPlotLeftAxisDirective,
-        BoxPlotWhiskerDirective,
-        BoxPlotUpperAntennaDirective,
-        BoxPlotLowerAntennaDirective,
-        BoxPlotBoxDirective,
-        BoxPlotMedianDirective,
-        BoxPlotOutlierDirective,
-        BoxPlotBrushSelectorDirective,
-        BoxPlotOutlierTooltipDirective,
-        ...components
+        ...components,
+        ...directives
     ],
     exports: [
-        BoxPlotBottomAxisDirective,
-        BoxPlotLeftAxisDirective,
-        BoxPlotWhiskerDirective,
-        BoxPlotUpperAntennaDirective,
-        BoxPlotLowerAntennaDirective,
-        BoxPlotBoxDirective,
-        BoxPlotMedianDirective,
-        BoxPlotOutlierDirective,
-        BoxPlotBrushSelectorDirective,
-        BoxPlotOutlierTooltipDirective,
-        ...components
+        ...components,
+        ...directives
     ]
 })
 export class DgpBoxPlotModule {
---
+++ projects/dgp-ng-charts/src/lib/box-plot/directives/directives.ts
@@ -0,0 +1,23 @@
+import { BoxPlotBottomAxisDirective } from "./box-plot-bottom-axis.directive";
+import { BoxPlotBoxDirective } from "./box-plot-box.directive";
+import { BoxPlotBrushSelectorDirective } from "./box-plot-brush-selector.directive";
+import { BoxPlotLeftAxisDirective } from "./box-plot-left-axis.directive";
+import { BoxPlotLowerAntennaDirective } from "./box-plot-lower-antenna.directive";
+import { BoxPlotMedianDirective } from "./box-plot-median.directive";
+import { BoxPlotOutlierDirective } from "./box-plot-outlier.directive";
+import { BoxPlotOutlierTooltipDirective } from "./box-plot-outlier-tooltip.directive";
+import { BoxPlotUpperAntennaDirective } from "./box-plot-upper-antenna.directive";
+import { BoxPlotWhiskerDirective } from "./box-plot-whisker.directive";
+
+export const directives = [
+    BoxPlotBottomAxisDirective,
+    BoxPlotBoxDirective,
+    BoxPlotBrushSelectorDirective,
+    BoxPlotLeftAxisDirective,
+    BoxPlotLowerAntennaDirective,
+    BoxPlotMedianDirective,
+    BoxPlotOutlierDirective,
+    BoxPlotOutlierTooltipDirective,
+    BoxPlotUpperAntennaDirective,
+    BoxPlotWhiskerDirective
+];
