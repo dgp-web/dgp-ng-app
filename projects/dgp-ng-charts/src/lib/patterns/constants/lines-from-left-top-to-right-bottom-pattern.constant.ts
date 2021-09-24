0009c3f9c4dfc21a1bf8d2a749ca506fbedcd73a
dgp <dgp-web@outlook.de>
2021-09-24 07:21:24 +0200

    Add horizontal constants for patterns

---
+++ projects/dgp-ng-charts/src/lib/patterns/constants/lines-from-left-top-to-right-bottom-pattern.constant.ts
@@ -0,0 +1,6 @@
+import { SVGPattern } from "../models";
+
+export const linesFromLeftTopToRightBottomPattern: SVGPattern = {
+    svgPatternId: "lines-from-left-top-to-right-bottom-pattern",
+    label: "Left top to right bottom"
+};
