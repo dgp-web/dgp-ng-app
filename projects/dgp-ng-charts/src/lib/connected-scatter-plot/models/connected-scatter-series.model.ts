145ad7aaec52d923d72e20e11c1167059719508a
dgp <dgp-web@outlook.de>
2021-09-28 10:40:48 +0200

    Add shape attribute to connected-scatter-series.model

--- projects/dgp-ng-charts/src/lib/connected-scatter-plot/models/connected-scatter-series.model.ts
+++ projects/dgp-ng-charts/src/lib/connected-scatter-plot/models/connected-scatter-series.model.ts
@@ -1,7 +1,12 @@
 import { Dot } from "./dot.model";
+import { Shape } from "../../symbols/models";
 
 export interface ConnectedScatterSeries {
     readonly connectedScatterSeriesId: string;
     readonly dots: ReadonlyArray<Dot>;
     readonly colorHex: string;
+    /**
+     * default value: "Circle"
+     */
+    readonly shape?: Shape;
 }
