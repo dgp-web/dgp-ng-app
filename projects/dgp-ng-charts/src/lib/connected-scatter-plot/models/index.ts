9063b6eaab9160553228e0b948f27f28bf178426
dgp <dgp-web@outlook.de>
2021-09-28 17:00:37 +0200

    Add model for ConnectedScatterPlotControlLine

--- projects/dgp-ng-charts/src/lib/connected-scatter-plot/models/index.ts
+++ projects/dgp-ng-charts/src/lib/connected-scatter-plot/models/index.ts
@@ -1,5 +1,6 @@
 export * from "./connected-scatter-group.model";
 export * from "./connected-scatter-plot.model";
 export * from "./connected-scatter-plot-config.model";
+export * from "./connected-scatter-plot-control-line.model";
 export * from "./connected-scatter-series.model";
 export * from "./dot.model";
