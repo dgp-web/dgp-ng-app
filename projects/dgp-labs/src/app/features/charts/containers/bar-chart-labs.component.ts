1f8f009d14a0ef610a4babfeb90d58b8936212d4
dgp-web <dgp-web@outlook.de>
2021-10-02 09:42:33 +0200

    Add pages

--- projects/dgp-labs/src/app/features/charts/containers/bar-chart-labs.component.ts
+++ projects/dgp-labs/src/app/features/charts/containers/bar-chart-labs.component.ts
@@ -5,8 +5,20 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
     template: `
         <dgp-page-header>
             <dgp-hamburger-menu-toggle></dgp-hamburger-menu-toggle>
-            Bar chart
+            Charts
         </dgp-page-header>
+
+        <dgp-docs-page>
+
+            <dgp-docs-page-content>
+
+                <dgp-docs-chapter-title>
+                    Bar chart
+                </dgp-docs-chapter-title>
+
+            </dgp-docs-page-content>
+        </dgp-docs-page>
+
     `,
     styles: [`
         :host {
--- projects/dgp-labs/src/app/features/charts/containers/box-plot-labs.component.ts
+++ projects/dgp-labs/src/app/features/charts/containers/box-plot-labs.component.ts
@@ -5,8 +5,20 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
     template: `
         <dgp-page-header>
             <dgp-hamburger-menu-toggle></dgp-hamburger-menu-toggle>
-            Box plot
+            Charts
         </dgp-page-header>
+
+        <dgp-docs-page>
+
+            <dgp-docs-page-content>
+
+                <dgp-docs-chapter-title>
+                    Box plot
+                </dgp-docs-chapter-title>
+
+            </dgp-docs-page-content>
+        </dgp-docs-page>
+
     `,
     styles: [`
         :host {
