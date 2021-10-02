729e0801f7d45e07e3b890a45035ceb4f1d38ea3
dgp-web <dgp-web@outlook.de>
2021-10-02 09:38:48 +0200

    Add sizing to pages

--- projects/dgp-labs/src/app/features/charts/containers/bar-chart-labs.component.ts
+++ projects/dgp-labs/src/app/features/charts/containers/bar-chart-labs.component.ts
@@ -1,11 +1,18 @@
-import { Component, ChangeDetectionStrategy } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
     selector: "dgp-bar-chart-labs",
     template: ``,
     styles: [`
+        :host {
+            display: flex;
+            flex-direction: column;
+            flex-grow: 1;
+            height: 100%;
+            overflow: auto;
+        }
 
-   `],
+    `],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BarChartLabsComponent {
--- projects/dgp-labs/src/app/features/charts/containers/connected-scatter-plot-labs.component.ts
+++ projects/dgp-labs/src/app/features/charts/containers/connected-scatter-plot-labs.component.ts
@@ -4,8 +4,15 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
     selector: "dgp-connected-scatter-plot-labs",
     template: ``,
     styles: [`
+        :host {
+            display: flex;
+            flex-direction: column;
+            flex-grow: 1;
+            height: 100%;
+            overflow: auto;
+        }
 
-   `],
+    `],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ConnectedScatterPlotLabsComponent {
