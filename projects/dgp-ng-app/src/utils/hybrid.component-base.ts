7a3d921e7255c9ff8cd46d16f5c7b53cc1173c4a
dgp-web <dgp-web@outlook.de>
2021-10-20 10:53:16 +0200

    Add DgpHybridComponentBase

--- projects/dgp-ng-app/src/utils/hybrid.component-base.ts
+++ projects/dgp-ng-app/src/utils/hybrid.component-base.ts
@@ -2,10 +2,12 @@ import { Directive } from "@angular/core";
 import { DgpModelEditorComponentBase } from "./model-editor.component-base";
 import { Store } from "@ngrx/store";
 import { Action, Selector } from "entity-store";
+import { notNullOrUndefined } from "./null-checking.functions";
+import { of } from "rxjs";
 
 @Directive()
 // tslint:disable-next-line:directive-class-suffix
-export abstract class HybridComponentBase<TModel, TState, TData> extends DgpModelEditorComponentBase<TModel> {
+export abstract class DgpHybridComponentBase<TModel, TState> extends DgpModelEditorComponentBase<TModel> {
 
     constructor(
         protected readonly store: Store<TState>
@@ -18,3 +20,18 @@ export abstract class HybridComponentBase<TModel, TState, TData> extends DgpMode
     readonly dispatch = (x: Action) => this.store.dispatch(x);
 
 }
+
+
+/**
+ * @deprecated This should not be used anymore. See DgpHybridComponentBase instead
+ */
+@Directive()
+// tslint:disable-next-line:directive-class-suffix
+export abstract class HybridComponentBase<TModel, TState, TData> extends DgpHybridComponentBase<TModel, TState> {
+
+    // tslint:disable-next-line:member-ordering
+    readonly data$ = notNullOrUndefined(this.getData) ? this.select(this.getData()) : of(null);
+
+    abstract getData?(): Selector<TState, TData>;
+}
+
