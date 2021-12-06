0ba610b835ef64b68cddc8136f6b0e807643970d
dgp-web <dgp-web@outlook.de>
2021-12-06 07:48:35 +0100

    Use observeProperty in view.component-base

--- projects/dgp-ng-app/src/utils/view.component-base.ts
+++ projects/dgp-ng-app/src/utils/view.component-base.ts
@@ -1,6 +1,5 @@
 import { Directive, Input } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
-import { isEqual } from "lodash";
+import { observeProperty } from "./observe-property.function";
 
 @Directive()
 // tslint:disable-next-line:directive-class-suffix
@@ -9,20 +8,9 @@ export abstract class DgpViewComponentBase<TModel> {
     @Input()
     disabled: boolean;
 
-    protected modelValue: TModel = null;
-    readonly model$ = new BehaviorSubject<TModel>(this.modelValue);
+    readonly model$ = observeProperty(this, "model");
 
     @Input()
-    get model(): TModel {
-        return this.modelValue;
-    }
-
-    set model(value: TModel) {
-
-        if (isEqual(value, this.modelValue)) return;
-
-        this.modelValue = value;
-        this.model$.next(value);
-    }
+    model: TModel;
 
 }
