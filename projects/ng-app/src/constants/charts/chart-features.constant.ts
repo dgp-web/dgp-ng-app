7f3a96f998d34151b16db013c6eec67de99b8960
dgp-web <dgp-web@outlook.de>
2021-12-08 06:39:16 +0100

    Add shapesFeature

--- projects/ng-app/src/constants/charts/chart-features.constant.ts
+++ projects/ng-app/src/constants/charts/chart-features.constant.ts
@@ -4,10 +4,12 @@ import { barChartFeature } from "./bar-chart-feature.constant";
 import { connectedScatterPlotFeature } from "./connected-scatter-plot-feature.constant";
 import { boxPlotFeature } from "./box-plot-feature.constant";
 import { heatmapFeature } from "./heatmap-feature.constant";
+import { shapesFeature } from "./shapes-feature.constant";
 
 export const chartFeatures: Many<AppFeature> = [
     barChartFeature,
     boxPlotFeature,
     connectedScatterPlotFeature,
-    heatmapFeature
+    heatmapFeature,
+    shapesFeature
 ];
---
+++ projects/ng-app/src/constants/charts/shapes-feature.constant.ts
@@ -0,0 +1,8 @@
+import { AppFeature } from "dgp-ng-app";
+
+export const shapesFeature: AppFeature = {
+    label: "Shapes",
+    matIconName: "star",
+    description: "Express data features with outlines.",
+    route: "/charts/shapes"
+};
