04ffb846226052f43d3375bb052e08b2f5a0adaa
dgp-web <dgp-web@outlook.de>
2021-12-08 06:35:02 +0100

    Add box-box-plot-feature.constant

---
+++ projects/ng-app/src/constants/charts/box-plot-feature.constant.ts
@@ -0,0 +1,8 @@
+import { AppFeature } from "../../models/app-feature.model";
+
+export const boxPlotFeature: AppFeature = {
+    label: "Boxplot",
+    matIconName: "candlestick_chart",
+    description: "Present statistics for data sets.",
+    route: "/charts/box-plot"
+};
--- projects/ng-app/src/constants/charts/chart-features.constant.ts
+++ projects/ng-app/src/constants/charts/chart-features.constant.ts
@@ -2,8 +2,10 @@ import {Many} from "data-modeling";
 import {AppFeature} from "../../models/app-feature.model";
 import {barChartFeature} from "./bar-chart-feature.constant";
 import {connectedScatterPlotFeature} from "./connected-scatter-plot-feature.constant";
+import { boxPlotFeature } from "./box-plot-feature.constant";
 
 export const chartFeatures: Many<AppFeature> = [
     barChartFeature,
+    boxPlotFeature,
     connectedScatterPlotFeature
 ];
