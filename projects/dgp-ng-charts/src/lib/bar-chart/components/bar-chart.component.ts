343f731e83dc1d23d8eef1f4a1d34d4f85332088
dgp-web <dgp-web@outlook.de>
2022-04-19 11:01:38 +0200

    Move DgpCardinalYAxisChartComponentBase to its own file

--- projects/dgp-ng-charts/src/lib/bar-chart/components/bar-chart.component.ts
+++ projects/dgp-ng-charts/src/lib/bar-chart/components/bar-chart.component.ts
@@ -1,7 +1,6 @@
-import { ChangeDetectionStrategy, Component, Directive, ElementRef, Inject, Input, ViewChild } from "@angular/core";
+import { ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild } from "@angular/core";
 import { filterNotNullOrUndefined, isNullOrUndefined, observeAttribute$ } from "dgp-ng-app";
 import { BarChart, BarGroups } from "../models";
-import { DgpChartComponentBase } from "../../chart/components/chart.component-base";
 import { BehaviorSubject, combineLatest } from "rxjs";
 import { debounceTime, map, shareReplay } from "rxjs/operators";
 import { createBarChartScales } from "../functions/create-bar-chart-scales.function";
@@ -12,60 +11,7 @@ import { getPlotRootTransform } from "../../shared/functions/get-plot-root-trans
 import { trackByBarGroupId } from "../functions/track-by-bar-group-id.function";
 import { trackByBarId } from "../functions/track-by-bar-id.function";
 import { defaultBarChartConfig } from "../constants";
-import { CardinalYAxis, ScaleType } from "../../shared/models";
-import { ID_PREFIX } from "../../shared/id-prefix-injection-token.constant";
-
-@Directive()
-// tslint:disable-next-line:directive-class-suffix
-export class DgpCardinalYAxisChartComponentBase extends DgpChartComponentBase {
-
-    @Input()
-    yAxisMin?: number;
-    readonly yAxisMin$ = observeAttribute$(this as DgpCardinalYAxisChartComponentBase, "yAxisMin");
-
-    @Input()
-    yAxisMax?: number;
-    readonly yAxisMax$ = observeAttribute$(this as DgpCardinalYAxisChartComponentBase, "yAxisMax");
-
-    @Input()
-    yAxisStep?: number;
-    readonly yAxisStep$ = observeAttribute$(this as DgpCardinalYAxisChartComponentBase, "yAxisStep");
-
-    @Input()
-    yAxisScaleType?: ScaleType;
-    readonly yAxisScaleType$ = observeAttribute$(this as DgpCardinalYAxisChartComponentBase, "yAxisScaleType");
-
-    @Input()
-    yAxisTickFormat?: (x: string) => string;
-    readonly yAxisTickFormat$ = observeAttribute$(this as DgpCardinalYAxisChartComponentBase, "yAxisTickFormat");
-
-    readonly yAxis$ = combineLatest([
-        this.yAxisMin$,
-        this.yAxisMax$,
-        this.yAxisStep$,
-        this.yAxisScaleType$,
-        this.yAxisTickFormat$,
-    ]).pipe(
-        map(x => ({
-            yAxisMin: x[0],
-            yAxisMax: x[1],
-            yAxisStep: x[2],
-            yAxisScaleType: x[3],
-            yAxisTickFormat: x[4]
-        } as CardinalYAxis))
-    );
-
-    readonly dataAreaClipPath = "url(#" + this.idPrefix + ".dataAreaClipPath" + ")";
-    readonly containerAreaClipPath = "url(#" + this.idPrefix + ".containerAreaClipPath" + ")";
-
-    constructor(
-        @Inject(ID_PREFIX)
-        protected readonly idPrefix: string
-    ) {
-        super();
-    }
-
-}
+import { DgpCardinalYAxisChartComponentBase } from "../../chart/components/cardinal-y-axis-chart.component-base";
 
 @Component({
     selector: "dgp-bar-chart",
