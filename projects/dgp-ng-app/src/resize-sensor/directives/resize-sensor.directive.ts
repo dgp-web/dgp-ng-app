ee4d0db6da0740134feacb22a551c49cfbdfcec1
dgp-web <dgp-web@outlook.de>
2022-04-20 16:12:19 +0200

    Update resize-sensor.directive.ts

--- projects/dgp-ng-app/src/resize-sensor/directives/resize-sensor.directive.ts
+++ projects/dgp-ng-app/src/resize-sensor/directives/resize-sensor.directive.ts
@@ -1,15 +1,13 @@
-import { AfterViewInit, Directive, ElementRef, EventEmitter, OnDestroy, Output } from "@angular/core";
-import { debounceTime, startWith, switchMap, tap } from "rxjs/operators";
-import { ResizeSensor } from "css-element-queries";
-import { from, interval, Subscription } from "rxjs";
-import { there } from "../../utils/there.function";
+import { AfterViewInit, Directive, ElementRef, EventEmitter, Input, NgZone, OnDestroy, Output } from "@angular/core";
+import { debounceTime, startWith } from "rxjs/operators";
+import { Subscription } from "rxjs";
 
 export interface Size {
     readonly width: number;
     readonly  height: number;
 }
 
-export function getInitialSizeFromElRef(payload: ElementRef): Size {
+export function getInitialSizeFromElRef(payload: ElementRef<HTMLDivElement>): Size {
     return {
         height: payload.nativeElement.clientHeight,
         width: payload.nativeElement.clientWidth
@@ -25,44 +23,29 @@ export function getInitialSizeFromElRef(payload: ElementRef): Size {
 })
 export class DgpResizeSensorDirective implements AfterViewInit, OnDestroy {
 
-    private resizeSensor: ResizeSensor;
-    private resizeObserver: ResizeObserver;
     private readonly resizeActionScheduler = new EventEmitter<Size>();
+
+    private resizeObserver: ResizeObserver;
     private resizeSubscription: Subscription;
-    private checkBrokenResizeSensorSubscription: Subscription;
-    private isInitialResize = true;
+
+    @Input()
+    stableTime = 250;
 
     @Output()
     readonly sizeChanged = new EventEmitter<Size>();
 
-    private readonly onResize = (size: Size) => this.scheduleResizeAction(size);
-
     constructor(
-        readonly elRef: ElementRef
+        private readonly elRef: ElementRef,
+        private readonly ngZone: NgZone
     ) {
-
     }
 
     ngAfterViewInit(): void {
 
         this.resizeSubscription = this.resizeActionScheduler.pipe(
             startWith(getInitialSizeFromElRef(this.elRef)),
-            debounceTime(250),
-            switchMap(size => from(this.doResize(size)))
-        )
-            .subscribe();
-
-        // TODO: mb trim
-        this.checkBrokenResizeSensorSubscription = interval(1000)
-            .pipe(
-                tap(() => {
-                    try {
-                        this.resizeSensor?.reset();
-                    } catch (e) {
-                    }
-                })
-            )
-            .subscribe();
+            debounceTime(this.stableTime)
+        ).subscribe(x => this.sizeChanged.next(x));
 
         this.initResizeSensor();
     }
@@ -71,48 +54,28 @@ export class DgpResizeSensorDirective implements AfterViewInit, OnDestroy {
         if (!this.resizeSubscription?.closed) {
             this.resizeSubscription?.unsubscribe();
         }
-        if (!this.checkBrokenResizeSensorSubscription?.closed) {
-            this.checkBrokenResizeSensorSubscription?.unsubscribe();
-        }
         if (this.resizeObserver) {
             this.resizeObserver.disconnect();
         }
     }
 
-    async doResize(size: Size): Promise<void> {
-        // this.resizeSensor.detach(this.onResize);
-        // await timer(0).toPromise();
-
-        this.sizeChanged.next(size);
-
-        // this.isInitialResize = true;
-        // this.resizeSensor = new ResizeSensor(this.elRef.nativeElement, this.onResize);
-    }
-
     protected scheduleResizeAction(size: Size): void {
-        if (this.isInitialResize) {
-            this.isInitialResize = false;
-            return;
-        }
         this.resizeActionScheduler.emit(size);
     }
 
     private initResizeSensor() {
-        if (there(this.resizeSensor) && there(this.onResize)) {
-            // this.resizeSensor.detach(this.onResize);
-        }
 
         this.resizeObserver = new ResizeObserver(entries => {
             const firstItem = entries[0];
-            this.onResize({
-                width: firstItem.contentRect.width,
-                height: firstItem.contentRect.height,
+            this.ngZone.run(() => {
+                this.scheduleResizeAction({
+                    width: firstItem.contentRect.width,
+                    height: firstItem.contentRect.height,
+                });
             });
         });
 
         this.resizeObserver.observe(this.elRef.nativeElement);
-
-        // this.resizeSensor = new ResizeSensor(this.elRef.nativeElement, this.onResize);
     }
 
 }
