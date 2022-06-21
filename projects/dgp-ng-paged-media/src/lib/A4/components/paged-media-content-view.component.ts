73e35304b2aeb77e55d1675265599d52e3e57cef
dgp-web <dgp-web@outlook.de>
2022-06-21 06:32:44 +0200

    Bind classlist

--- projects/dgp-ng-paged-media/src/lib/A4/components/paged-media-content-view.component.ts
+++ projects/dgp-ng-paged-media/src/lib/A4/components/paged-media-content-view.component.ts
@@ -8,26 +8,34 @@ import { HTMLPageContent } from "../../engine/models";
         <ng-container *ngFor="let item of model.itemsOnPage">
             <ng-container [ngSwitch]="item.tagName">
                 <p *ngSwitchCase="'P'"
-                   [innerHTML]="item.innerHTML | safe:'html'"></p>
+                   [innerHTML]="item.innerHTML | safe:'html'"
+                   [classList]="item.classList"></p>
 
                 <table *ngSwitchCase="'TABLE'"
                        [innerHTML]="item.innerHTML | safe:'html'"
                        [classList]="item.classList"></table>
 
                 <h1 *ngSwitchCase="'H1'"
-                    [innerHTML]="item.innerHTML | safe:'html'"></h1>
+                    [innerHTML]="item.innerHTML | safe:'html'"
+                    [classList]="item.classList"></h1>
                 <h2 *ngSwitchCase="'H2'"
-                    [innerHTML]="item.innerHTML | safe:'html'"></h2>
+                    [innerHTML]="item.innerHTML | safe:'html'"
+                    [classList]="item.classList"></h2>
                 <h3 *ngSwitchCase="'H3'"
-                    [innerHTML]="item.innerHTML | safe:'html'"></h3>
+                    [innerHTML]="item.innerHTML | safe:'html'"
+                    [classList]="item.classList"></h3>
                 <h4 *ngSwitchCase="'H4'"
-                    [innerHTML]="item.innerHTML | safe:'html'"></h4>
+                    [innerHTML]="item.innerHTML | safe:'html'"
+                    [classList]="item.classList"></h4>
                 <h5 *ngSwitchCase="'H5'"
-                    [innerHTML]="item.innerHTML | safe:'html'"></h5>
+                    [innerHTML]="item.innerHTML | safe:'html'"
+                    [classList]="item.classList"></h5>
                 <h6 *ngSwitchCase="'H6'"
-                    [innerHTML]="item.innerHTML | safe:'html'"></h6>
+                    [innerHTML]="item.innerHTML | safe:'html'"
+                    [classList]="item.classList"></h6>
                 <div *ngSwitchCase="'DIV'"
-                     [innerHTML]="item.innerHTML | safe:'html'"></div>
+                     [innerHTML]="item.innerHTML | safe:'html'"
+                     [classList]="item.classList"></div>
             </ng-container>
         </ng-container>
     `,
