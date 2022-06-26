4957eca9fcb1a2845f468c2c1be7289299a0fa61
dgp-web <dgp-web@outlook.de>
2022-06-26 13:56:35 +0200

    Rename index in track-by-connected-scatter-dot.pipe

--- projects/dgp-ng-charts/src/lib/connected-scatter-plot/pipes/track-by-connected-scatter-dot.pipe.ts
+++ projects/dgp-ng-charts/src/lib/connected-scatter-plot/pipes/track-by-connected-scatter-dot.pipe.ts
@@ -1,13 +1,12 @@
 import { Pipe, PipeTransform, TrackByFunction } from "@angular/core";
-import { ConnectedScatterSeries } from "../models";
-import { Point } from "../../shapes/models";
+import { ConnectedScatterSeries, Dot } from "../models";
 
 @Pipe({name: "trackByConnectedScatterDot"})
 export class TrackByConnectedScatterDotPipe implements PipeTransform {
 
-    transform<T>(series: ConnectedScatterSeries): TrackByFunction<any> {
-        return (outlierIndex: number, item: Point) => {
-            return series.connectedScatterSeriesId + "." + outlierIndex;
+    transform<T>(series: ConnectedScatterSeries): TrackByFunction<Dot> {
+        return (index: number, item: Dot) => {
+            return series.connectedScatterSeriesId + "." + index;
         };
     }
 }
