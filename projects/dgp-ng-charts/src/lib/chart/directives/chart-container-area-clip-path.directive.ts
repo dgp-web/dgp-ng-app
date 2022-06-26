225e0565dd38a81885ee0e81d50c38c728e1edc3
dgp-web <dgp-web@outlook.de>
2022-06-26 14:54:12 +0200

    Optimize DgpChartContainerAreaClipPathDirective

--- projects/dgp-ng-charts/src/lib/chart/directives/chart-container-area-clip-path.directive.ts
+++ projects/dgp-ng-charts/src/lib/chart/directives/chart-container-area-clip-path.directive.ts
@@ -1,13 +1,15 @@
-import { Directive, ElementRef, Inject, Input, OnChanges, Renderer2, SimpleChanges } from "@angular/core";
+import { AfterViewInit, Directive, ElementRef, Inject, Input, OnChanges, Renderer2, SimpleChanges } from "@angular/core";
 import { AxisScales } from "../../shared/models";
 import { ID_PREFIX } from "../../shared/id-prefix-injection-token.constant";
-import { notNullOrUndefined } from "dgp-ng-app";
+import { isNullOrUndefined, notNullOrUndefined } from "dgp-ng-app";
 
 @Directive({selector: "[dgpChartContainerAreaClipPath]"})
-export class DgpChartContainerAreaClipPathDirective implements OnChanges {
+export class DgpChartContainerAreaClipPathDirective implements AfterViewInit, OnChanges {
 
     @Input()
     scales: AxisScales;
+
+    private rect: SVGRectElement;
 
     constructor(private readonly elementRef: ElementRef<SVGClipPathElement>,
                 private readonly renderer: Renderer2,
@@ -15,27 +17,37 @@ export class DgpChartContainerAreaClipPathDirective implements OnChanges {
                 protected readonly idPrefix: string) {
     }
 
+    ngAfterViewInit(): void {
+        this.renderer.setAttribute(this.elementRef.nativeElement, "id", this.idPrefix + ".dataAreaClipPath");
+        this.renderer.setAttribute(this.elementRef.nativeElement, "fill", "transparent");
+        const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect") as SVGRectElement;
+
+        this.renderer.setAttribute(rect, "x", "0");
+        this.renderer.setAttribute(rect, "y", "0");
+
+        this.elementRef.nativeElement.appendChild(rect);
+        this.rect = rect;
+        this.trySetSizeOfRect();
+    }
+
     ngOnChanges(changes: SimpleChanges): void {
         if (changes.scales) {
-            this.renderer.setAttribute(this.elementRef.nativeElement, "id", this.idPrefix + ".containerAreaClipPath");
-            this.renderer.setAttribute(this.elementRef.nativeElement, "fill", "transparent");
+            this.trySetSizeOfRect();
+        }
+    }
 
-            const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect") as SVGRectElement;
+    private trySetSizeOfRect() {
+        if (isNullOrUndefined(this.rect)) return;
+        if (isNullOrUndefined(this.scales)) return;
 
-            this.renderer.setAttribute(rect, "x", "0");
-            this.renderer.setAttribute(rect, "y", "0");
-            const width = (this.scales.containerWidth).toString();
-            if (notNullOrUndefined(width) && !width.includes("NaN") && this.scales.containerWidth > 0) {
-                this.renderer.setAttribute(rect, "width", this.scales.containerWidth.toString());
-            }
+        const width = (this.scales.containerWidth).toString();
+        if (notNullOrUndefined(width) && !width.includes("NaN") && this.scales.containerWidth > 0) {
+            this.renderer.setAttribute(this.rect, "width", this.scales.containerWidth.toString());
+        }
 
-            const height = (this.scales.containerHeight).toString();
-            if (notNullOrUndefined(height) && !height.includes("NaN") && this.scales.containerHeight > 0) {
-                this.renderer.setAttribute(rect, "height", this.scales.containerHeight.toString());
-            }
-
-            this.elementRef.nativeElement.innerHTML = "";
-            this.elementRef.nativeElement.appendChild(rect);
+        const height = (this.scales.containerHeight).toString();
+        if (notNullOrUndefined(height) && !height.includes("NaN") && this.scales.containerHeight > 0) {
+            this.renderer.setAttribute(this.rect, "height", this.scales.containerHeight.toString());
         }
 
     }
