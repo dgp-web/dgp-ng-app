79b85b9fc94ef1d8e573bf597d98ba82dc7271a9
dgp-web <dgp-web@outlook.de>
2022-06-26 14:11:39 +0200

    Resolve series before drawing and avoid multiple scales$ bindings

--- projects/dgp-ng-charts/src/lib/connected-scatter-plot/components/connected-scatter-plot.component.ts
+++ projects/dgp-ng-charts/src/lib/connected-scatter-plot/components/connected-scatter-plot.component.ts
@@ -9,10 +9,41 @@ import {
     trackByConnectedScatterGroupId,
     trackByConnectedScatterSeriesId
 } from "../constants";
-import { filterNotNullOrUndefined, notNullOrUndefined, observeAttribute$, Size } from "dgp-ng-app";
-import { Shape } from "../../shapes/models";
+import { filterNotNullOrUndefined, isNullOrUndefined, notNullOrUndefined, observeAttribute$, Size } from "dgp-ng-app";
 import { idPrefixProvider } from "../../shared/id-prefix-provider.constant";
 import { DgpCardinalXYAxisChartComponentBase } from "../../chart/components/cardinal-xy-axis-chart.component-base";
+import { Many } from "data-modeling";
+import { Shape } from "../../shapes/models";
+
+export function resolveConnectedScatterGroups(payload: Many<ConnectedScatterGroup>): Many<ConnectedScatterGroup> {
+    if (isNullOrUndefined(payload)) return null;
+
+    return payload.map(group => {
+        return {
+            ...group,
+            series: group.series.map(series => {
+                return {
+                    ...series,
+                    shape: notNullOrUndefined(series.shape)
+                        ? series.shape
+                        : notNullOrUndefined(group.shape)
+                            ? group.shape
+                            : Shape.Circle,
+                    showVertices: notNullOrUndefined(series.showVertices)
+                        ? series.showVertices
+                        : notNullOrUndefined(group.showVertices)
+                            ? group.showVertices
+                            : true,
+                    showEdges: notNullOrUndefined(series.showEdges)
+                        ? series.showEdges
+                        : notNullOrUndefined(group.showEdges)
+                            ? group.showEdges
+                            : true
+                };
+            })
+        };
+    });
+}
 
 @Component({
     selector: "dgp-connected-scatter-plot",
@@ -29,26 +60,26 @@ import { DgpCardinalXYAxisChartComponentBase } from "../../chart/components/card
                 <ng-content select="[right-legend]"></ng-content>
             </ng-container>
 
-            <ng-container *ngIf="scales$ | async">
+            <ng-container *ngIf="scales$ | async as scales">
 
                 <svg:line xmlns:svg="http://www.w3.org/2000/svg"
                           *ngFor="let controlLine of controlLines; trackBy: trackByConnectedPlotControlLineId"
                           dgpConnectedScatterPlotControlLine
-                          [scales]="scales$ | async"
+                          [scales]="scales"
                           [connectedScatterPlotControlLine]="controlLine"></svg:line>
 
                 <svg:g xmlns:svg="http://www.w3.org/2000/svg"
-                       *ngFor="let group of model; trackBy: trackByConnectedScatterGroupId">
+                       *ngFor="let group of resolvedModel$ | async; trackBy: trackByConnectedScatterGroupId">
                     <ng-container *ngFor="let series of group.series; trackBy: trackByConnectedScatterSeriesId">
 
-                        <path *ngIf="showEdges(group, series)"
+                        <path *ngIf="series.showEdges"
                               dgpLineChartLine
                               [series]="series"
                               [group]="group"
-                              [scales]="scales$ | async"></path>
+                              [scales]="scales"></path>
 
                         <ng-container *ngFor="let dot of series.dots; trackBy: (series | trackByConnectedScatterDot)">
-                            <ng-container *ngIf="showVertices(group, series)">
+                            <ng-container *ngIf="series.showVertices">
 
                                 <g *ngIf="showDotTooltips; else noTooltip"
                                    [matTooltip]="getTooltip(group, series, dot)"
@@ -56,9 +87,9 @@ import { DgpCardinalXYAxisChartComponentBase } from "../../chart/components/card
                                    [dot]="dot"
                                    [series]="series"
                                    [group]="group"
-                                   [scales]="scales$ | async"
+                                   [scales]="scales"
                                    dgpDot
-                                   [model]="getShape(group, series)">
+                                   [model]="series.shape">
                                 </g>
 
                                 <ng-template #noTooltip>
@@ -66,9 +97,9 @@ import { DgpCardinalXYAxisChartComponentBase } from "../../chart/components/card
                                        [dot]="dot"
                                        [series]="series"
                                        [group]="group"
-                                       [scales]="scales$ | async"
+                                       [scales]="scales"
                                        dgpDot
-                                       [model]="getShape(group, series)">
+                                       [model]="series.shape">
                                     </g>
                                 </ng-template>
 
@@ -110,6 +141,7 @@ export class DgpConnectedScatterPlotComponent extends DgpCardinalXYAxisChartComp
     @Input()
     model: readonly ConnectedScatterGroup[];
     readonly model$ = observeAttribute$(this as DgpConnectedScatterPlotComponent, "model");
+    readonly resolvedModel$ = this.model$.pipe(map(resolveConnectedScatterGroups), shareReplay(1));
 
     @Input()
     controlLines?: ReadonlyArray<ConnectedScatterPlotControlLine>;
@@ -126,7 +158,7 @@ export class DgpConnectedScatterPlotComponent extends DgpCardinalXYAxisChartComp
         this.autoResize
             ? this.size$.pipe(filterNotNullOrUndefined())
             : this.size$.pipe(filterNotNullOrUndefined(), take(1)),
-        this.model$,
+        this.resolvedModel$,
         this.xAxis$,
         this.yAxis$,
         this.controlLines$
@@ -159,30 +191,6 @@ export class DgpConnectedScatterPlotComponent extends DgpCardinalXYAxisChartComp
         return group.connectedScatterGroupId
             + "." + series.connectedScatterSeriesId
             + "." + dot.x + "." + dot.y;
-    }
-
-    getShape(group: ConnectedScatterGroup, series: ConnectedScatterSeries): Shape {
-        if (notNullOrUndefined(series.shape)) {
-            return series.shape;
-        }
-        if (notNullOrUndefined(group.shape)) {
-            return group.shape;
-        }
-        return null;
-    }
-
-    showEdges(group: ConnectedScatterGroup, series: ConnectedScatterSeries): boolean {
-        let result = true;
-        if (notNullOrUndefined(group.showEdges)) result = group.showEdges;
-        if (notNullOrUndefined(series.showEdges)) result = series.showEdges;
-        return result;
-    }
-
-    showVertices(group: ConnectedScatterGroup, series: ConnectedScatterSeries): boolean {
-        let result = true;
-        if (notNullOrUndefined(group.showVertices)) result = group.showVertices;
-        if (notNullOrUndefined(series.showVertices)) result = series.showVertices;
-        return result;
     }
 
     getTooltip(group: ConnectedScatterGroup, series: ConnectedScatterSeries, dot: Dot) {
