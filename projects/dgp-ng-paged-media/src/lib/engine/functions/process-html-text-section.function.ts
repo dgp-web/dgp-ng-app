f025c46e1123ff5fcc065d44454684e0b7cc1dc3
dgp-web <dgp-web@outlook.de>
2022-08-29 11:43:40 +0200

    Update logic for extracting container items

--- projects/dgp-ng-paged-media/src/lib/engine/functions/process-html-text-section.function.ts
+++ projects/dgp-ng-paged-media/src/lib/engine/functions/process-html-text-section.function.ts
@@ -2,6 +2,7 @@ import { HTMLSection, PageContentSize, PagedHTMLComputationEngine } from "../mod
 import { extractHTMLItemsFromSection } from "./extract-html-items-from-section.function";
 import { checkHeight } from "./check-height.function";
 import { getOuterHeight } from "./get-outer-height.function";
+import { createHTMLWrapperElement } from "./create-html-wrapper-element.function";
 
 export function processHTMLTextSection(payload: {
     readonly engine: PagedHTMLComputationEngine;
@@ -12,23 +13,42 @@ export function processHTMLTextSection(payload: {
     const htmlSection = payload.htmlSection;
     const pageContentSize = payload.pageContentSize;
 
+    const refElement = htmlSection.nativeElement;
     const htmlItems = extractHTMLItemsFromSection(htmlSection);
-    htmlItems.forEach(htmlItem => {
-        htmlItem.style.width = pageContentSize.width + pageContentSize.widthUnit;
 
-        const height = getOuterHeight(htmlItem);
-        checkHeight({height, pageContentSize});
-
-        if (height <= engine.currentPageRemainingHeight) {
-            engine.currentPage.itemsOnPage.push(htmlItem);
-            engine.currentPageRemainingHeight -= height;
-        } else {
-            engine.finishPage();
-
-            engine.currentPage.itemsOnPage.push(htmlItem);
-            engine.currentPageRemainingHeight -= height;
-        }
+    let div = createHTMLWrapperElement("div", pageContentSize);
+    refElement.classList.forEach(x => {
+        if (x !== "dgp-hide-in-print") div.classList.add(x);
     });
 
-}
+    htmlItems.forEach(htmlItem => {
+        const helpDiv = createHTMLWrapperElement("div", pageContentSize);
+        helpDiv.appendChild(htmlItem);
 
+        const height = getOuterHeight(div);
+        const height02 = getOuterHeight(helpDiv);
+        checkHeight({height, pageContentSize});
+
+        if (height + height02 <= engine.currentPageRemainingHeight) {
+            div.appendChild(htmlItem);
+        } else {
+            engine.currentPage.itemsOnPage.push(div);
+            engine.finishPage();
+
+            document.body.removeChild(div);
+            div = createHTMLWrapperElement("div", pageContentSize);
+            refElement.classList.forEach(x => {
+                if (x !== "dgp-hide-in-print") div.classList.add(x);
+            });
+            div.appendChild(htmlItem);
+        }
+
+        document.body.removeChild(helpDiv);
+    });
+
+    engine.currentPage.itemsOnPage.push(div);
+    const height1 = getOuterHeight(div);
+    engine.currentPageRemainingHeight -= height1;
+    document.body.removeChild(div);
+
+}
