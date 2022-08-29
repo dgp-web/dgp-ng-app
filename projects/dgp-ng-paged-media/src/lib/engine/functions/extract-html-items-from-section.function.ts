fbb665afff0e5d8afe38fdec85bc0c3f9a981ef5
dgp-web <dgp-web@outlook.de>
2022-08-29 10:53:33 +0200

    Start updating extractHTMLItemsFromTextSection

--- projects/dgp-ng-paged-media/src/lib/engine/functions/extract-html-items-from-section.function.ts
+++ projects/dgp-ng-paged-media/src/lib/engine/functions/extract-html-items-from-section.function.ts
@@ -2,7 +2,7 @@ import { HTMLSection } from "../models";
 import { extractHTMLItemsFromTableSection } from "./extract-html-items-from-table-section.function";
 import { extractHTMLItemsFromTextSection } from "./extract-html-items-from-text-section.function";
 
-export function extractHTMLItemsFromSection(payload: HTMLSection): NodeListOf<HTMLElement> {
+export function extractHTMLItemsFromSection(payload: HTMLSection): NodeListOf<HTMLElement | Element> {
     switch (payload.type) {
         case "text":
             return extractHTMLItemsFromTextSection(payload.nativeElement);
--- projects/dgp-ng-paged-media/src/lib/engine/functions/extract-html-items-from-text-section.function.ts
+++ projects/dgp-ng-paged-media/src/lib/engine/functions/extract-html-items-from-text-section.function.ts
@@ -1,4 +1,4 @@
 export function extractHTMLItemsFromTextSection(payload: HTMLElement) {
-    return payload.querySelectorAll("p");
+    return payload.querySelectorAll("p, ul, ol, pre, h1, h2, h3, h4, h5, h6");
 }
 
