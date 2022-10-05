ade2c5c6d7dbfc59f4d706ce45fb94cf665ecce6
dgp-web <dgp-web@outlook.de>
2022-10-05 14:37:00 +0200

    Add fabric types

--- projects/dgp-ng-image-editor/src/lib/image-editor/components/image-editor.component.ts
+++ projects/dgp-ng-image-editor/src/lib/image-editor/components/image-editor.component.ts
@@ -1,6 +1,80 @@
 import { ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild } from "@angular/core";
-import { AngleType, Transform } from "../../models";
-import { observeAttribute$ } from "dgp-ng-app";
+import { AngleType, ImageRegion, Transform } from "../../models";
+import { DgpModelEditorComponentBase, notNullOrUndefined, observeAttribute$ } from "dgp-ng-app";
+import { fabric } from "fabric";
+
+export function isCanvasValid(canvas: fabric.Canvas) {
+    return notNullOrUndefined(canvas)
+        && notNullOrUndefined(canvas.getContext());
+}
+
+export function getFabricImageFromUrl$(imageUrl: string): Promise<fabric.Image> {
+    return new Promise<fabric.Image>(resolve => {
+        fabric.Image.fromURL(imageUrl, image => resolve(image));
+    });
+}
+
+export function renderImageToCanvas$(payload: {
+    readonly image: fabric.Image,
+    readonly canvas: fabric.Canvas
+}): Promise<void> {
+
+    const scaleX = payload.canvas.getWidth() / payload.image.width;
+    const scaleY = payload.canvas.getHeight() / payload.image.height;
+
+    return new Promise<void>(resolve => {
+
+        if (isCanvasValid(payload.canvas)) payload.canvas.clear();
+
+        payload.canvas.setBackgroundImage(payload.image, () => {
+            resolve();
+        }, {
+            lockRotation: true,
+            scaleX,
+            scaleY
+        });
+
+    });
+
+}
+
+export interface CreateRectPayload {
+    readonly imageRegion: ImageRegion;
+    readonly canvas: fabric.Canvas;
+}
+
+export function createRect(payload: CreateRectPayload): fabric.Rect {
+    const imageSegment = payload.imageRegion;
+    const canvas = payload.canvas;
+
+    let referenceWidth = 1;
+    let referenceHeight = 1;
+
+   /* if (imageSegment.isNormalized) {
+        referenceWidth = canvas.getWidth();
+        referenceHeight = canvas.getHeight();
+    }
+*/
+    const width = imageSegment.width * referenceWidth;
+    const height = imageSegment.height * referenceHeight;
+    const left = imageSegment.offsetX * referenceWidth;
+    const top = imageSegment.offsetY * referenceHeight;
+
+    return new fabric.Rect({
+        strokeWidth: 1,
+        stroke: "black",
+        fill: "rgba(0,0,0,0)",
+        width,
+        height,
+        left,
+        top,
+        data: imageSegment,
+        lockRotation: true,
+        lockScalingFlip: true,
+        lockSkewingX: true,
+        lockSkewingY: true
+    });
+}
 
 @Component({
     selector: "dgp-image-editor",
@@ -19,14 +93,13 @@ import { observeAttribute$ } from "dgp-ng-app";
     `],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DgpImageEditorComponent implements Transform {
+export class DgpImageEditorComponent extends DgpModelEditorComponentBase<string> implements Transform {
+
+    private currentFabricCanvas: fabric.Canvas;
+    private rectsRef: ReadonlyArray<fabric.Rect>;
 
     @ViewChild("canvas", {static: true})
     readonly canvasElement: ElementRef;
-
-    @Input()
-    src: string;
-    readonly src$ = observeAttribute$(this as DgpImageEditorComponent, "src");
 
     @Input()
     offsetX: number;
@@ -59,5 +132,116 @@ export class DgpImageEditorComponent implements Transform {
     @Input()
     scaleY: number;
     readonly scaleY$ = observeAttribute$(this as DgpImageEditorComponent, "scaleY");
+
+    async setupFabric$() {
+        if (this.model) {
+            await this.tryCreateFabric();
+            return this.draw$();
+        } else {
+            return this.tryDestroyFabric$();
+        }
+
+    }
+
+    private draw$(): Promise<void> {
+        if (!this.currentFabricCanvas || !this.model) return Promise.resolve();
+
+
+        this.unregisterListeners();
+        return this.drawThingsOntoCanvas$(this.model, this.currentFabricCanvas);
+    }
+
+    async drawThingsOntoCanvas$(
+        imageUrl: string,
+        canvas: fabric.Canvas
+    ): Promise<void> {
+
+        if (!isCanvasValid(canvas)) return;
+
+        let image: fabric.Image;
+
+        try {
+            image = await getFabricImageFromUrl$(imageUrl);
+        } catch (e) {
+            console.error(e);
+            return Promise.resolve();
+        }
+
+        try {
+            await renderImageToCanvas$({canvas, image});
+        } catch (e) {
+            console.error(e);
+            return Promise.resolve();
+        }
+/*
+        const rects = imageSegments.map(imageSegment => {
+            let fabricRect: fabric.Rect;
+
+            fabricRect = createRect({imageSegment, canvas});
+
+            if (!imageSegment.isVisible) {
+                fabricRect = fabricRect.set({
+                    stroke: "",
+                    opacity: 0,
+                    selectable: false,
+                    hoverCursor: "default"
+                });
+            }
+
+            // Disables selection of segments when logged out
+            if (this.disabled) {
+                fabricRect.selectable = false;
+                fabricRect.hoverCursor = "default";
+            }
+
+            return fabricRect;
+        });*/
+/*
+
+        rects.forEach(rect => {
+            rect.on("modified", this.rectUpdateHandler);
+            if (isCanvasValid(canvas)) canvas.add(rect);
+        });
+*/
+
+        if (isCanvasValid(canvas)) canvas.renderAll();
+    }
+
+    private async tryCreateFabric() {
+        await this.tryDestroyFabric$();
+
+        this.currentFabricCanvas = new fabric.Canvas(this.canvasElement.nativeElement, {
+            selection: false,
+            renderOnAddRemove: true
+        });
+
+        this.currentFabricCanvas.renderAll();
+    }
+/*
+
+    rectUpdateHandler = (e: fabric.IEvent) => {
+        const rect = e.target as fabric.Rect;
+        const imageSegment = e.target.data as ImageSegment;
+        const canvas = e.target.canvas as fabric.Canvas;
+
+        const updatedImageSegment = resolveImageSegment({rect, canvas, imageSegment});
+        this.setImageSegments(updatedImageSegment);
+    }
+*/
+
+    private tryDestroyFabric$() {
+        this.unregisterListeners();
+        if (this.currentFabricCanvas) {
+            this.currentFabricCanvas.clear();
+            this.currentFabricCanvas.dispose();
+            this.currentFabricCanvas = null;
+        }
+        return Promise.resolve();
+    }
+
+    private unregisterListeners() {
+        if (!this.rectsRef) return;
+        this.rectsRef.forEach(rect => rect.off("modified"));
+    }
 
 }
