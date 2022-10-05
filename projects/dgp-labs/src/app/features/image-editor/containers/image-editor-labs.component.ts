74ea31e1a74c1aa2bbd9ef71b6fcc90978e9749b
dgp-web <dgp-web@outlook.de>
2022-10-05 13:15:10 +0200

    Prepare layout

--- projects/dgp-labs/src/app/features/image-editor/containers/image-editor-labs.component.ts
+++ projects/dgp-labs/src/app/features/image-editor/containers/image-editor-labs.component.ts
@@ -1,11 +1,38 @@
-import { Component, ChangeDetectionStrategy } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
     selector: "dgp-image-editor-labs",
-    template: ``,
+    template: `
+        <dgp-page-header>
+            <dgp-hamburger-menu-toggle></dgp-hamburger-menu-toggle>
+            Image editor
+        </dgp-page-header>
+
+        <dgp-docking-layout>
+            <dgp-docking-layout-item type="row">
+                <dgp-docking-layout-item type="column"
+                                         [width]="80">
+                    <dgp-docking-layout-container label="Image">
+                        <ng-template>
+
+                        </ng-template>
+                    </dgp-docking-layout-container>
+                </dgp-docking-layout-item>
+
+                <dgp-docking-layout-item type="column"
+                                         [width]="20">
+                    <dgp-docking-layout-container label="Settings">
+                        <ng-template>
+
+                        </ng-template>
+                    </dgp-docking-layout-container>
+                </dgp-docking-layout-item>
+            </dgp-docking-layout-item>
+        </dgp-docking-layout>
+    `,
     styles: [`
 
-   `],
+    `],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ImageEditorLabsComponent {
--- projects/dgp-labs/src/app/features/image-editor/image-editor-labs.module.ts
+++ projects/dgp-labs/src/app/features/image-editor/image-editor-labs.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from "@angular/core";
 import { containers } from "./containers/containers";
 import { RouterModule } from "@angular/router";
 import { ImageEditorLabsComponent } from "./containers/image-editor-labs.component";
+import { DgpHamburgerMenuToggleModule, DgpPageHeaderModule } from "dgp-ng-app";
+import { DgpDockingLayoutModule } from "dgp-ng-docking-layout";
 
 @NgModule({
     imports: [
         RouterModule.forChild([{
             path: "image-editor",
             component: ImageEditorLabsComponent
-        }])
+        }]),
+        DgpPageHeaderModule,
+        DgpHamburgerMenuToggleModule,
+        DgpDockingLayoutModule
     ],
     declarations: [
         ...containers
