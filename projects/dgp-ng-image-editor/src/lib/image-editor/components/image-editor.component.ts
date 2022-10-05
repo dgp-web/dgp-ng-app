3ac71aa8b47fc74a51db6b5b5aa08c0b8d2749e8
dgp-web <dgp-web@outlook.de>
2022-10-05 14:27:53 +0200

    Add inputs to image editor component

--- projects/dgp-ng-image-editor/src/lib/image-editor/components/image-editor.component.ts
+++ projects/dgp-ng-image-editor/src/lib/image-editor/components/image-editor.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from "@angular/core";
+import { ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild } from "@angular/core";
+import { AngleType, Transform } from "../../models";
+import { observeAttribute$ } from "dgp-ng-app";
 
 @Component({
     selector: "dgp-image-editor",
@@ -17,11 +19,45 @@ import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from "@angu
     `],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DgpImageEditorComponent {
+export class DgpImageEditorComponent implements Transform {
 
     @ViewChild("canvas", {static: true})
     readonly canvasElement: ElementRef;
 
+    @Input()
+    src: string;
+    readonly src$ = observeAttribute$(this as DgpImageEditorComponent, "src");
 
+    @Input()
+    offsetX: number;
+    readonly offsetX$ = observeAttribute$(this as DgpImageEditorComponent, "offsetX");
+
+    @Input()
+    offsetY: number;
+    readonly offsetY$ = observeAttribute$(this as DgpImageEditorComponent, "offsetY");
+
+    @Input()
+    rotateX: number;
+    readonly rotateX$ = observeAttribute$(this as DgpImageEditorComponent, "rotateX");
+
+    @Input()
+    rotateY: number;
+    readonly rotateY$ = observeAttribute$(this as DgpImageEditorComponent, "rotateY");
+
+    @Input()
+    rotationAngle: number;
+    readonly rotationAngle$ = observeAttribute$(this as DgpImageEditorComponent, "rotationAngle");
+
+    @Input()
+    rotationAngleType: AngleType;
+    readonly rotationAngleType$ = observeAttribute$(this as DgpImageEditorComponent, "rotationAngleType");
+
+    @Input()
+    scaleX: number;
+    readonly scaleX$ = observeAttribute$(this as DgpImageEditorComponent, "scaleX");
+
+    @Input()
+    scaleY: number;
+    readonly scaleY$ = observeAttribute$(this as DgpImageEditorComponent, "scaleY");
 
 }
