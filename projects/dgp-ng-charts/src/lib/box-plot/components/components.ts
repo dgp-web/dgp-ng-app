9f65097d39f244eaae71a82259498ab8b3f6a9de
dgp-web <dgp-web@outlook.de>
2022-10-10 10:27:14 +0200

    Add empty DgpHybridBoxPlotComponent

--- projects/dgp-ng-charts/src/lib/box-plot/components/components.ts
+++ projects/dgp-ng-charts/src/lib/box-plot/components/components.ts
@@ -1,7 +1,9 @@
 import { DgpBoxPlotComponent } from "./box-plot.component";
 import { DgpSVGBoxPlotComponent } from "./svg-box-plot.component";
+import { DgpHybridBoxPlotComponent } from "./hybrid-box-plot.component";
 
 export const components = [
     DgpBoxPlotComponent,
+    DgpHybridBoxPlotComponent,
     DgpSVGBoxPlotComponent
 ];
---
+++ projects/dgp-ng-charts/src/lib/box-plot/components/hybrid-box-plot.component.ts
@@ -0,0 +1,13 @@
+import { Component, ChangeDetectionStrategy } from "@angular/core";
+
+@Component({
+    selector: "dgp-hybrid-box-plot",
+    template: ``,
+    styles: [`
+
+   `],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DgpHybridBoxPlotComponent {
+
+}
