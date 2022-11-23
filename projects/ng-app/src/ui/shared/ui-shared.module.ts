f31f70734f5f0ec173c5ba2fda32688db1dd4c49
dgp-web <dgp-web@outlook.de>
2022-11-23 13:37:04 +0100

    Use MatLegacyListModule in ui-shared module

--- projects/ng-app/src/ui/shared/ui-shared.module.ts
+++ projects/ng-app/src/ui/shared/ui-shared.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from "@angular/core";
 import * as dgp from "dgp-ng-app";
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatListModule } from "@angular/material/list";
 import { MatIconModule } from "@angular/material/icon";
+import { MatLegacyListModule } from "@angular/material/legacy-list";
 
 const sharedModules = [
     MatButtonModule,
     MatIconModule,
-    MatListModule,
+    MatLegacyListModule,
     MatToolbarModule,
 
     dgp.DgpEmptyStateModule
