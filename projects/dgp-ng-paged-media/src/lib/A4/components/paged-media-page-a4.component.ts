dff0dba72bcdd25742749a5627d1ca0eb5745faa
dgp-web <dgp-web@outlook.de>
2023-01-02 17:08:20 +0100

    Remove tslint comments from A4 components

--- projects/dgp-ng-paged-media/src/lib/A4/components/paged-media-page-a4.component.ts
+++ projects/dgp-ng-paged-media/src/lib/A4/components/paged-media-page-a4.component.ts
@@ -1,7 +1,6 @@
 import { Component } from "@angular/core";
 
 @Component({
-    // tslint:disable-next-line:component-selector
     selector: "dgp-paged-media-page-A4",
     template: `
         <ng-content></ng-content>
--- projects/dgp-ng-paged-media/src/public-api.ts
+++ projects/dgp-ng-paged-media/src/public-api.ts
@@ -2,16 +2,18 @@
  * Public API Surface of dgp-ng-paged-media
  */
 
+export * from "./lib/engine/constants";
+export * from "./lib/engine/functions";
+export * from "./lib/engine/models";
+
+export { DgpPagedMediaA4Module } from "./lib/A4/paged-media-a4.module";
 export { DgpPagedMediaContentViewComponent } from "./lib/A4/components/paged-media-content-view.component";
 export { DgpPagedMediaContentComponent } from "./lib/A4/components/paged-media-content.component";
 export { DgpPagedMediaFooterComponent } from "./lib/A4/components/paged-media-footer.component";
 export { DgpPagedMediaHeaderComponent } from "./lib/A4/components/paged-media-header.component";
 export { DgpPagedMediaPageA4Component } from "./lib/A4/components/paged-media-page-a4.component";
 export { DgpPagedMediaSectionA4Component } from "./lib/A4/components/paged-media-section-a4.component";
-export { DgpPagedMediaA4Module } from "./lib/A4/paged-media-a4.module";
 
-export * from "./lib/engine/constants";
-export * from "./lib/engine/functions";
-export * from "./lib/engine/models";
 export { OffscreenRenderer } from "./lib/engine/services/offscreen-renderer.service";
+
 
