71c9e996d23c971153ee7187cfe2253dc268b8ba
dgp-web <dgp-web@outlook.de>
2023-01-02 17:16:53 +0100

    Try direct imports

--- projects/dgp-ng-paged-media/src/lib/A4/paged-media-a4.module.ts
+++ projects/dgp-ng-paged-media/src/lib/A4/paged-media-a4.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core";
-import { components } from "./components/components";
 import { CommonModule } from "@angular/common";
 import { SafePipeModule } from "dgp-ng-app";
+import { DgpPagedMediaContentViewComponent } from "./components/paged-media-content-view.component";
+import { DgpPagedMediaContentComponent } from "./components/paged-media-content.component";
+import { DgpPagedMediaFooterComponent } from "./components/paged-media-footer.component";
+import { DgpPagedMediaHeaderComponent } from "./components/paged-media-header.component";
+import { DgpPagedMediaPageA4Component } from "./components/paged-media-page-a4.component";
+import { DgpPagedMediaSectionA4Component } from "./components/paged-media-section-a4.component";
 
 
 @NgModule({
@@ -10,10 +15,20 @@ import { SafePipeModule } from "dgp-ng-app";
         SafePipeModule
     ],
     declarations: [
-        ...components
+        DgpPagedMediaContentViewComponent,
+        DgpPagedMediaContentComponent,
+        DgpPagedMediaFooterComponent,
+        DgpPagedMediaHeaderComponent,
+        DgpPagedMediaPageA4Component,
+        DgpPagedMediaSectionA4Component
     ],
     exports: [
-        ...components
+        DgpPagedMediaContentViewComponent,
+        DgpPagedMediaContentComponent,
+        DgpPagedMediaFooterComponent,
+        DgpPagedMediaHeaderComponent,
+        DgpPagedMediaPageA4Component,
+        DgpPagedMediaSectionA4Component
     ]
 })
 export class DgpPagedMediaA4Module {
