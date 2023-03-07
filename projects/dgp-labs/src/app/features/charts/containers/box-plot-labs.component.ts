8a645288fb701fdfb4e4123d3707544b03802b21
dgp-web <dgp-web@outlook.de>
2023-03-07 08:29:31 +0100

    Add empty page for box plot

--- projects/dgp-labs/src/app/features/charts/containers/box-plot-labs.component.ts
+++ projects/dgp-labs/src/app/features/charts/containers/box-plot-labs.component.ts
@@ -1,23 +1,41 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ModelMetadata } from "data-modeling";
+import { BoxPlot, BoxPlotRenderer } from "dgp-ng-charts";
+import { DgpModelEditorComponentBase } from "dgp-ng-app";
+
+export const boxPlotMetadata: ModelMetadata<BoxPlot> = {
+    label: "Box plot",
+
+    attributes: {}
+};
+
+export const testBoxPlot: BoxPlot = {
+    model: []
+};
+
 
 @Component({
     selector: "dgp-box-plot-labs",
     template: `
         <dgp-page-header>
             <dgp-hamburger-menu-toggle></dgp-hamburger-menu-toggle>
-            Charts
+            {{boxPlotMetadata.label}}
         </dgp-page-header>
 
-        <dgp-docs-page>
+        <dgp-split-panel orientation="horizontal">
 
-            <dgp-docs-page-content>
+            <dgp-split-panel-content [size]="80">
+                <ng-template>
+                    <dgp-box-plot [model]="model.model"
+                                  [renderer]="renderer"></dgp-box-plot>
+                </ng-template>
+            </dgp-split-panel-content>
 
-                <dgp-docs-chapter-title>
-                    Box plot
-                </dgp-docs-chapter-title>
+            <dgp-split-panel-content [size]="20">
+                <ng-template></ng-template>
+            </dgp-split-panel-content>
 
-            </dgp-docs-page-content>
-        </dgp-docs-page>
+        </dgp-split-panel>
 
     `,
     styles: [`
@@ -28,10 +46,18 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
             height: 100%;
             overflow: auto;
         }
-
     `],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BoxPlotLabsComponent {
+export class BoxPlotLabsComponent extends DgpModelEditorComponentBase<BoxPlot> {
+
+    readonly boxPlotMetadata = boxPlotMetadata;
+
+    renderer = BoxPlotRenderer.Hybrid;
+    model = testBoxPlot;
+
+    updateRenderer(renderer: BoxPlotRenderer) {
+        this.renderer = renderer;
+    }
 
 }
