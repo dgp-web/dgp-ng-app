20a3053f476855a46110da4eba4a67c88521e2ec
dgp-web <dgp-web@outlook.de>
2023-03-31 20:40:18 +0200

    Remove unneeded event-handling logic from component

--- projects/dgp-ng-docking-layout/src/lib/custom-goldenlayout/components/component.component.ts
+++ projects/dgp-ng-docking-layout/src/lib/custom-goldenlayout/components/component.component.ts
@@ -3,15 +3,9 @@ import { ItemContainerComponent } from "./grid/item-container.component";
 import { ChangeDetectionStrategy, Component, Directive, ElementRef, forwardRef, Inject } from "@angular/core";
 import { DockingLayoutService } from "../docking-layout.service";
 import { ComponentConfiguration, ITEM_CONFIG, ItemConfiguration, itemDefaultConfig, PARENT_ITEM_COMPONENT } from "../types";
-import { BubblingEvent, EventEmitter, LayoutManagerUtilities } from "../utilities";
-import { stateChangedEventType } from "../constants/event-types/state-changed-event-type.constant";
+import { EventEmitter } from "../utilities";
 import { RootComponent } from "./root.component";
-import { ALL_EVENT } from "../constants/event-types/all-event.constant";
 import { AreaSides } from "../models/area.model";
-import { beforeItemDestroyedEventType } from "../constants/event-types/before-item-destroyed-event-type.constant";
-import { itemDestroyedEventType } from "../constants/event-types/item-destroyed-event-type.constant";
-import { itemCreatedEventType } from "../constants/event-types/item-created-event-type.constant";
-import { createItemTypeCreatedEventType } from "../functions/create-item-type-created-event-type.function";
 
 @Directive()
 // tslint:disable-next-line:directive-class-suffix
@@ -19,10 +13,6 @@ export abstract class ComponentAbstractContentItemComponent extends EventEmitter
 
     isInitialised = false;
     element: JQuery;
-    childElementContainer: JQuery;
-
-    pendingEventPropagations = {};
-    throttledEvents = [stateChangedEventType];
 
     protected constructor(
         readonly dockingLayoutService: DockingLayoutService,
@@ -32,7 +22,6 @@ export abstract class ComponentAbstractContentItemComponent extends EventEmitter
         super();
 
         this.config = {...itemDefaultConfig, ...config};
-        this.on(ALL_EVENT, this.propagateEvent, this);
     }
 
     callDownwards(functionName: string,
@@ -47,10 +36,6 @@ export abstract class ComponentAbstractContentItemComponent extends EventEmitter
         }
     }
 
-    remove() {
-        this.parent.removeChild(this as any);
-    }
-
     _$setParent(parent: AbstractContentItemComponent) {
         this.parent = parent;
     }
@@ -60,72 +45,25 @@ export abstract class ComponentAbstractContentItemComponent extends EventEmitter
     }
 
     hide() {
-        this.callOnActiveComponents("hide");
         this.element.hide();
         this.dockingLayoutService.updateSize();
     }
 
     show() {
-        this.callOnActiveComponents("show");
         this.element.show();
         this.dockingLayoutService.updateSize();
     }
 
-    private callOnActiveComponents(methodName: string): void {
-    }
-
     destroy() {
         this.unsubscribe();
-        this.emitBubblingEvent(beforeItemDestroyedEventType);
         this.callDownwards("destroy", [], true, true);
         this.element.remove();
-        this.emitBubblingEvent(itemDestroyedEventType);
     }
 
     init(): void {
-
         this.isInitialised = true;
-        this.emitBubblingEvent(itemCreatedEventType);
-        this.emitBubblingEvent(createItemTypeCreatedEventType(this.config.type));
     }
 
-    emitBubblingEvent(name: string) {
-        const event = new BubblingEvent(name, this);
-        this.emit(name, event);
-    }
-
-    private propagateEvent(name: string, event: BubblingEvent) {
-        if (event instanceof BubblingEvent &&
-            event.isPropagationStopped === false &&
-            this.isInitialised === true) {
-
-            if (this.parent) {
-                (this.parent as AbstractContentItemComponent).emit?.apply(this.parent, Array.prototype.slice.call(arguments, 0));
-            } else {
-                this.scheduleEventPropagationToLayoutManager(name, event);
-            }
-        }
-    }
-
-    private scheduleEventPropagationToLayoutManager(name: string, event) {
-        if (new LayoutManagerUtilities().indexOf(name, this.throttledEvents) === -1) {
-            this.dockingLayoutService.emit(name, event.origin);
-        } else {
-            if (this.pendingEventPropagations[name] !== true) {
-                this.pendingEventPropagations[name] = true;
-                new LayoutManagerUtilities().animFrame(() => this.propagateEventToLayoutManager(name, event));
-            }
-        }
-
-    }
-
-    /**
-     * Callback for events scheduled by _scheduleEventPropagationToLayoutManager
-     */
-    private propagateEventToLayoutManager(name: string, event) {
-        this.pendingEventPropagations[name] = false;
-        this.dockingLayoutService.emit(name, event);
-    }
 }
 
 @Component({
