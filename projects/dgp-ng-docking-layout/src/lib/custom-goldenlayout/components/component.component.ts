d4d83e3dfca141348104bb08268eda79179493e8
dgp-web <dgp-web@outlook.de>
2023-03-31 20:46:38 +0200

    Get rid of ComponentAbstractContentItemComponent

--- projects/dgp-ng-docking-layout/src/lib/custom-goldenlayout/components/component.component.ts
+++ projects/dgp-ng-docking-layout/src/lib/custom-goldenlayout/components/component.component.ts
@@ -1,27 +1,76 @@
 import { AbstractContentItemComponent } from "./shared/abstract-content-item.component";
 import { ItemContainerComponent } from "./grid/item-container.component";
-import { ChangeDetectionStrategy, Component, Directive, ElementRef, forwardRef, Inject } from "@angular/core";
+import { ChangeDetectionStrategy, Component, ElementRef, forwardRef, Inject } from "@angular/core";
 import { DockingLayoutService } from "../docking-layout.service";
-import { ComponentConfiguration, ITEM_CONFIG, ItemConfiguration, itemDefaultConfig, PARENT_ITEM_COMPONENT } from "../types";
+import { ComponentConfiguration, ITEM_CONFIG, itemDefaultConfig, PARENT_ITEM_COMPONENT } from "../types";
 import { EventEmitter } from "../utilities";
-import { RootComponent } from "./root.component";
 import { AreaSides } from "../models/area.model";
 
-@Directive()
-// tslint:disable-next-line:directive-class-suffix
-export abstract class ComponentAbstractContentItemComponent extends EventEmitter {
-
+@Component({
+    selector: "dgp-gl-component",
+    template: ``,
+    styles: [`
+        :host {
+            overflow: auto;
+        }
+    `],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class GlComponent extends EventEmitter {
+    private container: ItemContainerComponent;
+    isComponent = true;
     isInitialised = false;
     element: JQuery;
-
-    protected constructor(
-        readonly dockingLayoutService: DockingLayoutService,
-        readonly config: ItemConfiguration,
-        public parent: AbstractContentItemComponent | RootComponent
+    
+    constructor(
+        @Inject(forwardRef(() => DockingLayoutService))
+        public dockingLayoutService: DockingLayoutService,
+        @Inject(ITEM_CONFIG)
+        public config: ComponentConfiguration,
+        @Inject(PARENT_ITEM_COMPONENT)
+        public parent: AbstractContentItemComponent,
+        private readonly elementRef: ElementRef<HTMLElement>,
     ) {
         super();
-
         this.config = {...itemDefaultConfig, ...config};
+        this.isComponent = true;
+
+        const vcRef = this.dockingLayoutService.getViewContainerRef();
+
+        const itemContainerComponentRef = vcRef.createComponent(ItemContainerComponent);
+        itemContainerComponentRef.instance.config = this.config;
+        itemContainerComponentRef.changeDetectorRef.markForCheck();
+        this.container = itemContainerComponentRef.instance;
+
+        this.element = $(this.elementRef.nativeElement);
+        // move rendered item to container (shouldn't be necessary?)
+        this.elementRef.nativeElement.append(itemContainerComponentRef.injector.get(ElementRef).nativeElement);
+    }
+
+    setSize() {
+    }
+
+    init() {
+        this.isInitialised = true;
+        this.container.emit("open");
+    }
+
+    hide() {
+        this.container.hide();
+        this.element.hide();
+        this.dockingLayoutService.updateSize();
+    }
+
+    show() {
+        this.container.show();
+        this.element.show();
+        this.dockingLayoutService.updateSize();
+    }
+
+    destroy() {
+        this.container.emit("destroy", this);
+        this.unsubscribe();
+        this.element.remove();
     }
 
     callDownwards(functionName: string,
@@ -42,90 +91,6 @@ export abstract class ComponentAbstractContentItemComponent extends EventEmitter
 
     highlightDropZone(x: number, y: number, area: AreaSides) {
         this.dockingLayoutService.dropTargetIndicator.highlightArea(area);
-    }
-
-    hide() {
-        this.element.hide();
-        this.dockingLayoutService.updateSize();
-    }
-
-    show() {
-        this.element.show();
-        this.dockingLayoutService.updateSize();
-    }
-
-    destroy() {
-        this.unsubscribe();
-        this.callDownwards("destroy", [], true, true);
-        this.element.remove();
-    }
-
-    init(): void {
-        this.isInitialised = true;
-    }
-
-}
-
-@Component({
-    selector: "dgp-gl-component",
-    template: ``,
-    styles: [`
-        :host {
-            overflow: auto;
-        }
-    `],
-    changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class GlComponent extends ComponentAbstractContentItemComponent {
-    private container: ItemContainerComponent;
-    isComponent = true;
-
-    constructor(
-        @Inject(forwardRef(() => DockingLayoutService))
-        public dockingLayoutService: DockingLayoutService,
-        @Inject(ITEM_CONFIG)
-        public config: ComponentConfiguration,
-        @Inject(PARENT_ITEM_COMPONENT)
-        public parent: AbstractContentItemComponent,
-        private readonly elementRef: ElementRef<HTMLElement>,
-    ) {
-        super(dockingLayoutService, config, parent);
-
-        this.isComponent = true;
-
-        const vcRef = this.dockingLayoutService.getViewContainerRef();
-
-        const itemContainerComponentRef = vcRef.createComponent(ItemContainerComponent);
-        itemContainerComponentRef.instance.config = this.config;
-        itemContainerComponentRef.changeDetectorRef.markForCheck();
-        this.container = itemContainerComponentRef.instance;
-
-        this.element = $(this.elementRef.nativeElement);
-        // move rendered item to container (shouldn't be necessary?)
-        this.elementRef.nativeElement.append(itemContainerComponentRef.injector.get(ElementRef).nativeElement);
-    }
-
-    setSize() {
-    }
-
-    init() {
-        super.init();
-        this.container.emit("open");
-    }
-
-    hide() {
-        this.container.hide();
-        super.hide();
-    }
-
-    show() {
-        this.container.show();
-        super.show();
-    }
-
-    destroy() {
-        this.container.emit("destroy", this);
-        super.destroy();
     }
 
 }
