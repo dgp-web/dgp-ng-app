746d59e8997f8c07a45136c36ae0e9961b1bb6de
dgp-web <dgp-web@outlook.de>
2023-05-11 08:04:08 +0200

    Implement probit function

--- projects/dgp-labs/src/app/features/charts/containers/__tests__/get-gaussian-density.function.spec.ts
+++ projects/dgp-labs/src/app/features/charts/containers/__tests__/get-gaussian-density.function.spec.ts
@@ -8,9 +8,8 @@ describe("gaussian", () => {
 
     describe(getGaussianProbabilityDensity.name, () => {
 
-        it(`playground`, () => {
+        it(`getGaussianProbabilityDensity`, () => {
 
-            //const payload = Array.from({length: 100}, (v, i) => i + 1);
             const payload = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
 
             console.log("payload", payload);
@@ -21,19 +20,14 @@ describe("gaussian", () => {
 
             console.log("result", result);
 
-            const result01 = result.map(x => 1 / x);
-
-            console.log("result01", result01);
-
         });
 
     });
 
     describe(getGaussianCumulativeDistribution.name, () => {
 
-        it(`playground`, () => {
+        it(`getGaussianCumulativeDistribution`, () => {
 
-            //const payload = Array.from({length: 100}, (v, i) => i + 1);
             const payload = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
 
             console.log("payload", payload);
@@ -44,37 +38,31 @@ describe("gaussian", () => {
 
             console.log("result", result);
 
-            const result01 = result.map(x => 1 / x);
-
-            console.log("result01", result01);
-
         });
 
     });
 
     describe(getGaussianQuantile.name, () => {
 
-        it(`playground`, () => {
+        fit(`getGaussianQuantile`, () => {
 
             const payload = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
-            //const payload = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
-            // const payload = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
             console.log("payload", payload);
 
-            const result = payload.map(x => getGaussianQuantile({
-                variance: 0.2,
-                median: 0,
-                p: getGaussianCumulativeDistribution({
-                    x, variance: 0.2, median: 0
-                })
+            const p = payload.map(x => getGaussianCumulativeDistribution({
+                x, variance: 0.2, median: 0
             }));
 
-            console.log("result", result);
+            console.log("p", p);
 
-            const result01 = result.map(x => 1 / x);
+            const result = p.map(pValue => getGaussianQuantile({
+                variance: 0.2,
+                median: 0,
+                p: pValue
+            }));
 
-            console.log("result01", result01);
+            console.log("quantiles", result);
 
         });
 
