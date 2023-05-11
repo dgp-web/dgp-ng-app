5d27bf7dcc85905cf7572d7a00ee015b83129123
dgp-web <dgp-web@outlook.de>
2023-05-11 07:35:25 +0200

    Add getGaussianQuantile

--- projects/dgp-labs/src/app/features/charts/containers/__tests__/get-gaussian-density.function.spec.ts
+++ projects/dgp-labs/src/app/features/charts/containers/__tests__/get-gaussian-density.function.spec.ts
@@ -1,4 +1,8 @@
-import { getGaussianCumulativeDistribution, getGaussianProbabilityDensity } from "../connected-scatter-plot-labs.component";
+import {
+    getGaussianCumulativeDistribution,
+    getGaussianProbabilityDensity,
+    getGaussianQuantile
+} from "../connected-scatter-plot-labs.component";
 
 describe("gaussian", () => {
 
@@ -27,7 +31,7 @@ describe("gaussian", () => {
 
     describe(getGaussianCumulativeDistribution.name, () => {
 
-        fit(`playground`, () => {
+        it(`playground`, () => {
 
             //const payload = Array.from({length: 100}, (v, i) => i + 1);
             const payload = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
@@ -36,6 +40,28 @@ describe("gaussian", () => {
 
             const result = payload.map(x => getGaussianCumulativeDistribution({
                 x, variance: 0.2, median: 0
+            }));
+
+            console.log("result", result);
+
+            const result01 = result.map(x => 1 / x);
+
+            console.log("result01", result01);
+
+        });
+
+    });
+
+    describe(getGaussianQuantile.name, () => {
+
+        fit(`playground`, () => {
+
+            const payload = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
+
+            console.log("payload", payload);
+
+            const result = payload.map(x => getGaussianQuantile({
+                x, variance: 0.2, median: 0, p: 0.517
             }));
 
             console.log("result", result);
--- projects/dgp-labs/src/app/features/charts/containers/connected-scatter-plot-labs.component.ts
+++ projects/dgp-labs/src/app/features/charts/containers/connected-scatter-plot-labs.component.ts
@@ -69,6 +69,59 @@ export function getGaussianCumulativeDistribution(payload: {
 
 }
 
+/**
+ * References
+ * - https://stackoverflow.com/questions/12556685/is-there-a-javascript-implementation-of-the-inverse-error-function-akin-to-matl
+ */
+export function getInverseGaussianError(x: number): number {
+    let z: number;
+    const a = 0.147;
+    let the_sign_of_x;
+    if (0 == x) {
+        the_sign_of_x = 0;
+    } else if (x > 0) {
+        the_sign_of_x = 1;
+    } else {
+        the_sign_of_x = -1;
+    }
+
+    if (0 !== x) {
+        const ln_1minus_x_sqrd = Math.log(1 - x * x);
+        const ln_1minusxx_by_a = ln_1minus_x_sqrd / a;
+        const ln_1minusxx_by_2 = ln_1minus_x_sqrd / 2;
+        const ln_etc_by2_plus2 = ln_1minusxx_by_2 + (2 / (Math.PI * a));
+        const first_sqrt = Math.sqrt((ln_etc_by2_plus2 * ln_etc_by2_plus2) - ln_1minusxx_by_a);
+        const second_sqrt = Math.sqrt(first_sqrt - ln_etc_by2_plus2);
+        z = second_sqrt * the_sign_of_x;
+    } else { // x is zero
+        z = 0;
+    }
+    return z;
+}
+
+/**
+ * The quantile function is the inverse of the CDF function
+ *
+ * References
+ * - https://en.wikipedia.org/wiki/Normal_distribution
+ */
+export function getGaussianQuantile(payload: {
+    readonly x: number;
+    readonly median: number;
+    readonly variance: number;
+    readonly p: number;
+}) {
+    const x = payload.x;
+
+    const median = payload.median;
+    const variance = payload.variance;
+    const scale = Math.sqrt(variance);
+
+    const p = payload.variance;
+
+    return median + scale * Math.sqrt(variance) * getInverseGaussianError(2 * p - 1);
+}
+
 @Component({
     selector: "dgp-connected-scatter-plot-labs",
     template: `
