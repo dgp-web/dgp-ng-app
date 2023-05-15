1c0425d7357e27562a7ba9695b3d001071a7d4bb
dgp-web <dgp-web@outlook.de>
2023-05-15 07:37:04 +0200

    Add yAxisInterpolator input

--- projects/dgp-ng-charts/src/lib/shared/models/cardinal-y-axis.model.ts
+++ projects/dgp-ng-charts/src/lib/shared/models/cardinal-y-axis.model.ts
@@ -2,6 +2,7 @@ import { ScaleType } from "./scale-type.model";
 import { CardinalAxisTickFormat } from "./cardinal-axis-tick-format.model";
 import { YAxisTitle } from "./y-axis-title.model";
 import { ShowYAxisGridLines } from "./show-y-axis-grid-lines.model";
+import * as d3 from "d3";
 
 export interface CardinalYAxis extends YAxisTitle, ShowYAxisGridLines {
     readonly yAxisMin?: number;
@@ -9,4 +10,5 @@ export interface CardinalYAxis extends YAxisTitle, ShowYAxisGridLines {
     readonly yAxisStep?: number;
     readonly yAxisScaleType?: ScaleType;
     readonly yAxisTickFormat?: CardinalAxisTickFormat;
+    readonly yAxisInterpolator?: d3.InterpolatorFactory<number, number>;
 }
