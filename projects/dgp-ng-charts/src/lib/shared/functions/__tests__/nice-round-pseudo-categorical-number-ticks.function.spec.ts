ec29a7b5b726c50b2a159957dac72237edceafab
dgp-web <dgp-web@outlook.de>
2023-08-08 07:06:04 +0200

    Refactor method

--- projects/dgp-ng-charts/src/lib/shared/functions/__tests__/nice-round-pseudo-categorical-number-ticks.function.spec.ts
+++ projects/dgp-ng-charts/src/lib/shared/functions/__tests__/nice-round-pseudo-categorical-number-ticks.function.spec.ts
@@ -52,7 +52,6 @@ describe("niceRoundPseudoCategoricalNumberTicks", () => {
 
                 const elements = x.split(".");
                 const decimalPlaces = elements[1];
-                // TODO: find three consecutive numbers
 
                 const trimmedDecimalPlaces = roundDecimalPlaces(decimalPlaces);
 
@@ -83,50 +82,54 @@ describe("niceRoundPseudoCategoricalNumberTicks", () => {
 
 });
 
-export function roundDecimalPlaces(decimalPlaces: string): string {
+export function canKeepLeadingZero(payload: {
+    readonly decimalPlace: string;
+    readonly result: string;
+}): boolean {
+    const decimalPlace = payload.decimalPlace;
+    const result = payload.result;
+
+    return decimalPlace === "0" && result.split("").every(x => x === "0");
+}
+
+export function roundDecimalPlaces(decimalPlaces: string, config = {
+    sequenceLength: 2
+}): string {
 
     let result = "";
     let count = 0;
+    const sequenceLength = config.sequenceLength;
 
-    for (let i = 0; i < decimalPlaces.length; i++) {
-        const char = decimalPlaces[i];
-        if (result === "") {
-            result += char;
-        } else {
+    for (const decimalPlace of decimalPlaces) {
 
-            if (char === "0" && result.split("").every(x => x === "0")) {
-                result += char;
-            } else {
-
-                if (char === result[result.length - 1]) {
-
-                    if (count === 2) {
-                        // take last item from string, and round it // 333 to 33 and 666 to 67
-                        const lastItemButOne = result[result.length - 2];
-                        result = result.split("")
-                            .filter((x, i1) => i1 < result.length - 2)
-                            .join("");
-                        if (+char >= 5) {
-                            result += (+lastItemButOne + 1).toString();
-                        } else {
-                            result += lastItemButOne.toString();
-                        }
-                        break;
-                    } else {
-                        result += char;
-                        count++;
-                    }
-
-                } else {
-                    result += char;
-                    count = 1;
-                }
-
-            }
-
+        if (result === "" || canKeepLeadingZero({decimalPlace, result})) {
+            result += decimalPlace;
+            continue;
         }
+
+        if (decimalPlace !== result[result.length - 1]) {
+            result += decimalPlace;
+            count = 1;
+            continue;
+        }
+
+        if (count !== sequenceLength) {
+            result += decimalPlace;
+            count++;
+            continue;
+        }
+
+        // take start item of sequence from string, and round it // 333 to 33 and 666 to 67
+        let lastItemButOne = +result[result.length - sequenceLength];
+        result = result.split("")
+            .filter((x, i1) => i1 < result.length - sequenceLength)
+            .join("");
+        if (+decimalPlace >= 5) lastItemButOne++;
+        result += lastItemButOne.toString();
+        break;
     }
 
     return result;
-
 }
+
+
