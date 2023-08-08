eb54805e6008f3fc2e5995a41c09f287739ac587
dgp-web <dgp-web@outlook.de>
2023-08-08 06:51:28 +0200

    Implement rounding as desired

--- projects/dgp-ng-charts/src/lib/shared/functions/__tests__/nice-round-pseudo-categorical-number-ticks.function.spec.ts
+++ projects/dgp-ng-charts/src/lib/shared/functions/__tests__/nice-round-pseudo-categorical-number-ticks.function.spec.ts
@@ -50,20 +50,27 @@ describe("niceRoundPseudoCategoricalNumberTicks", () => {
                 const parsedNumber = +x;
                 if (parsedNumber > 10) return x;
 
-                return parsedNumber.toPrecision(3);
+                const elements = x.split(".");
+                const decimalPlaces = elements[1];
+                // TODO: find three consecutive numbers
+
+                const trimmedDecimalPlaces = roundDecimalPlaces(decimalPlaces);
+
+
+                return elements[0] + "." + trimmedDecimalPlaces;
 
             })
             .map(x => x + "x");
 
         const expectedOutput = [
             "0x",
-            "0.00278x",
-            "0.00833x",
-            "0.0167x",
+            "0.0028x",
+            "0.0083x",
+            "0.017x",
             "0.05x",
-            "0.167x",
-            "0.333x",
-            "0.667x",
+            "0.17x",
+            "0.33x",
+            "0.67x",
             "1x",
             "1.33x",
             "1.67x",
@@ -75,3 +82,51 @@ describe("niceRoundPseudoCategoricalNumberTicks", () => {
     });
 
 });
+
+export function roundDecimalPlaces(decimalPlaces: string): string {
+
+    let result = "";
+    let count = 0;
+
+    for (let i = 0; i < decimalPlaces.length; i++) {
+        const char = decimalPlaces[i];
+        if (result === "") {
+            result += char;
+        } else {
+
+            if (char === "0" && result.split("").every(x => x === "0")) {
+                result += char;
+            } else {
+
+                if (char === result[result.length - 1]) {
+
+                    if (count === 2) {
+                        // take last item from string, and round it // 333 to 33 and 666 to 67
+                        const lastItemButOne = result[result.length - 2];
+                        result = result.split("")
+                            .filter((x, i1) => i1 < result.length - 2)
+                            .join("");
+                        if (+char >= 5) {
+                            result += (+lastItemButOne + 1).toString();
+                        } else {
+                            result += lastItemButOne.toString();
+                        }
+                        break;
+                    } else {
+                        result += char;
+                        count++;
+                    }
+
+                } else {
+                    result += char;
+                    count = 1;
+                }
+
+            }
+
+        }
+    }
+
+    return result;
+
+}
