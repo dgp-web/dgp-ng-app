e4c437ff53fe339e64f57b05bb1c886d0db046c1
dgp-web <dgp-web@outlook.de>
2023-08-17 04:44:48 +0200

    Adjust hosting for version 16

--- projects/ng-app/src/app/app.component.ts
+++ projects/ng-app/src/app/app.component.ts
@@ -12,7 +12,7 @@ import { coreFeatures } from "../constants/core-features.constant";
             <dgp-hamburger-menu dgp-hamburger-menu>
 
                 <dgp-hamburger-menu-header>
-                    DGP NG APP
+                    DGP NG APP (v16)
                 </dgp-hamburger-menu-header>
 
                 <dgp-hamburger-menu-entries>
