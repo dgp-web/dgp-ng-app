74fd506a7d3e854216016c418c93c05f0d046832
dgp-web <dgp-web@outlook.de>
2023-08-29 07:17:16 +0200

    Test addOverflowRows

--- projects/dgp-ng-paged-media/src/lib/engine/functions/table/add-overflow-rows.function.ts
+++ projects/dgp-ng-paged-media/src/lib/engine/functions/table/add-overflow-rows.function.ts
@@ -1,12 +1,28 @@
 import { Many } from "data-modeling";
 import { OverflowRow } from "./create-overflow-row.function";
 
+export function insertAfter(newNode: Node, existingNode: Node) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
+
 export function addOverflowRows(payload: {
     readonly overflowRows: Many<OverflowRow>;
     readonly table: HTMLTableElement;
 }): HTMLTableElement {
     const table = payload.table;
     const overflowRows = payload.overflowRows;
+    const originalRows = table.querySelectorAll("tr");
+
+    originalRows.forEach((originalRow, index) => {
+
+        overflowRows.forEach(overflowRow => {
+
+            if (index === overflowRow.originalRowIndex) {
+                insertAfter(overflowRow.tableRow, originalRow);
+            }
+
+        });
+    });
 
     return table;
 }
