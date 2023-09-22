484dbc981d930606451f9193e4136c37d8a2aa0a
dgp-web <dgp-web@outlook.de>
2023-09-22 06:35:43 +0200

    Start getOverflowingColumnsInfo

--- projects/dgp-ng-paged-media/src/lib/engine/functions/table/get-overflowing-columns-info.function.ts
+++ projects/dgp-ng-paged-media/src/lib/engine/functions/table/get-overflowing-columns-info.function.ts
@@ -1,5 +1,5 @@
 import { PageContentSize } from "../../models";
-import { Many } from "data-modeling";
+import { Many, Mutable } from "data-modeling";
 
 export interface OverflowingColumnsInfo {
     readonly columnKeys?: Many<string>;
@@ -10,5 +10,34 @@ export function getOverflowingColumnsInfo(payload: {
     readonly table: HTMLTableElement;
     readonly pageContentSize: PageContentSize;
 }): OverflowingColumnsInfo {
-    return null;
+
+    const table = payload.table;
+    const pageContentSize = payload.pageContentSize;
+
+    const result: Mutable<OverflowingColumnsInfo> = {
+        columnKeys: [],
+        lastVisibleColumnIndex: null
+    };
+
+    const utilityTable = document.createElement("table");
+
+    const headerRow = document.createElement("tr");
+    utilityTable.appendChild(headerRow);
+
+    /**
+     * Iterate over the header cells
+     */
+    table.querySelector("tr")
+        .querySelectorAll("th")
+        .forEach((th, columnIndex) => {
+
+            table.querySelectorAll("tr").forEach((regularRow, regularRowIndex) => {
+                if (regularRowIndex === 0) return;
+
+                
+            });
+
+        });
+
+    return result;
 }
