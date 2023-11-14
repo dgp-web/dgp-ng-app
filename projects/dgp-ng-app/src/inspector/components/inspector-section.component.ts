149f8031ee38eb773e871cb571dd86f2968c1129
dgp-web <dgp-web@outlook.de>
2023-11-14 21:00:33 +0100

    PARTIAL: Experiment

--- projects/dgp-ng-app/src/inspector/components/inspector-section.component.ts
+++ projects/dgp-ng-app/src/inspector/components/inspector-section.component.ts
@@ -4,26 +4,67 @@ import { AttributeMetadata } from "data-modeling";
 @Component({
     selector: "dgp-inspector-section",
     template: `
-        <h3 class="label-item"
-            mat-subheader>
-            <span class="label">
-            {{ label || metadata?.label }}
-            </span>
-            <mat-icon style="margin-left: 8px;"
-                      class="section-icon mat-icon--small">
-                {{matIconName || metadata?.icon}}
-            </mat-icon>
-            <dgp-spacer></dgp-spacer>
-            <ng-content select="[actions]"></ng-content>
-            <dgp-expansion-toggle *ngIf="expandable"
-                                  [model]="expanded"
-                                  (modelChange)="updateExpanded($event)"></dgp-expansion-toggle>
-        </h3>
-        <ng-container *ngIf="expanded">
+        <details>
+            <summary>
+                <div class="summary-content">
+                    <span class="label">
+                    {{ label || metadata?.label }}
+                    </span>
+                    <mat-icon style="margin-left: 8px;"
+                              class="section-icon mat-icon--small">
+                        {{matIconName || metadata?.icon}}
+                    </mat-icon>
+                    <dgp-spacer></dgp-spacer>
+                    <ng-content select="[actions]"></ng-content>
+                </div>
+            </summary>
             <ng-content></ng-content>
-        </ng-container>
+        </details>
+
+
+        <!--  <h3 class="label-item"
+              mat-subheader>
+              <span class="label">
+              {{ label || metadata?.label }}
+              </span>
+              <mat-icon style="margin-left: 8px;"
+                        class="section-icon mat-icon&#45;&#45;small">
+                  {{matIconName || metadata?.icon}}
+              </mat-icon>
+              <dgp-spacer></dgp-spacer>
+              <ng-content select="[actions]"></ng-content>
+              <dgp-expansion-toggle *ngIf="expandable"
+                                    [model]="expanded"
+                                    (modelChange)="updateExpanded($event)"></dgp-expansion-toggle>
+          </h3>
+          <ng-container *ngIf="expanded">
+              <ng-content></ng-content>
+          </ng-container>-->
     `,
     styles: [`
+        details > summary {
+            list-style: none;
+            display: flex;
+        }
+
+        summary::marker {
+            display: none;
+        }
+
+        summary::after {
+            content: ' ►';
+        }
+
+        details[open] summary:after {
+            content: " ▼";
+        }
+
+        .summary-content {
+            display: flex;
+            align-items: center;
+            flex-grow: 1;
+        }
+
         .label-item {
             border-bottom-width: 1px;
             border-bottom-style: solid;
