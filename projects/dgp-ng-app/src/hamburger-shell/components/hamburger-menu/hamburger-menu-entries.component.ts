48ec4817e5f36338d28435da8cb211765284f01c
dgp-web <dgp-web@outlook.de>
2024-01-05 06:19:33 +0100

    Add custom styling for hamburger-entry

--- projects/dgp-ng-app/src/hamburger-shell/components/hamburger-menu/hamburger-menu-entries.component.ts
+++ projects/dgp-ng-app/src/hamburger-shell/components/hamburger-menu/hamburger-menu-entries.component.ts
@@ -3,19 +3,13 @@ import { Component, ChangeDetectionStrategy } from "@angular/core";
 @Component({
     selector: "dgp-hamburger-menu-entries",
     template: `
-        <mat-nav-list>
-            <ng-content></ng-content>
-        </mat-nav-list>
+        <ng-content></ng-content>
     `,
     styles: [`
         :host {
             display: flex;
             flex-direction: column;
             overflow: auto;
-            flex-grow: 1;
-        }
-
-        mat-nav-list {
             flex-grow: 1;
         }
     `],
--- projects/dgp-ng-app/src/hamburger-shell/components/hamburger-menu/hamburger-menu-entry.component.ts
+++ projects/dgp-ng-app/src/hamburger-shell/components/hamburger-menu/hamburger-menu-entry.component.ts
@@ -4,7 +4,6 @@ import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
     selector: "dgp-hamburger-menu-entry",
     template: `
         <a *ngIf="isNonEmptyRoute(); else emptyRoute"
-           mat-list-item
            [routerLink]="route"
            [routerLinkActive]="'dgp-list-item--selected'"
            [class.disabled]="disabled"
@@ -14,8 +13,7 @@ import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
         </a>
 
         <ng-template #emptyRoute>
-            <a mat-list-item
-               class="disabled"
+            <a class="disabled"
                tabindex="-1">
                 <mat-icon>{{ matIconName }}</mat-icon>
                 {{ label }}
@@ -23,14 +21,33 @@ import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
         </ng-template>
     `,
     styles: [`
+        :host {
+            display: flex;
+            width: 100%;
+            padding-left: 8px;
+            padding-right: 8px;
+        }
+
+        a {
+            text-decoration: none;
+            color: inherit;
+            display: flex;
+            align-items: center;
+            flex-grow: 1;
+            padding-top: 4px;
+            padding-bottom: 4px;
+        }
+
         mat-icon {
-            margin-right: 16px;
+            margin-left: 8px;
+            margin-right: 8px;
         }
 
         .disabled {
             pointer-events: none;
             color: gray !important;
         }
+
     `],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
