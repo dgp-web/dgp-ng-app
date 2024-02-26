c6e8fa6b7c4cd89c3428be15603c77752f655792
dgp-web <dgp-web@outlook.de>
2024-02-26 22:39:52 +0100

    Adjust probabilityPlotTickFormat

--- projects/dgp-ng-charts/src/lib/shared/constants/normal/probability-plot-tick-format.constant.ts
+++ projects/dgp-ng-charts/src/lib/shared/constants/normal/probability-plot-tick-format.constant.ts
@@ -4,5 +4,10 @@ import * as d3 from "d3";
 export const probabilityPlotTickFormat: CardinalAxisTickFormat = (x: number) => {
     if (x >= 1 && x <= 99) return d3.format("d")(x);
     if (x < 1) return x.toPrecision(1);
-    return x.toString();
+    /**
+     * x > 99
+     *
+     * This replaces issues like 99.999000001
+     */
+    return x.toString().replace(/(0+1+)+/, "");
 };
