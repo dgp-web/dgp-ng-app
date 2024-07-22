f5d907d31a1130b79bf5de54ac05da04de19a2f3
dgp-web <dgp-web@outlook.de>
2024-07-22 11:25:54 +0200

    Add ViewPrimitiveDocsSectionComponent

--- projects/ng-app/src/features/core/component-primitives-docs/components/components.ts
+++ projects/ng-app/src/features/core/component-primitives-docs/components/components.ts
@@ -1,5 +1,7 @@
 import { ComponentPrimitiveOverviewTableComponent } from "./component-primitive-overview-table.component";
+import { ViewPrimitiveDocsSectionComponent } from "./view-primitive-docs-section.component";
 
 export const components = [
-    ComponentPrimitiveOverviewTableComponent
+    ComponentPrimitiveOverviewTableComponent,
+    ViewPrimitiveDocsSectionComponent
 ];
