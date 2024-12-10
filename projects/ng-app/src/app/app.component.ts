2ec5f683e4d209155b6e8f7a2a9b9d1c2dddaba6
dgp-web <dgp-web@outlook.de>
2024-12-10 06:08:01 +0100

    Prepare deployment to firebase with task

--- projects/ng-app/src/app/app.component.ts
+++ projects/ng-app/src/app/app.component.ts
@@ -12,7 +12,7 @@ import { coreFeatures } from "../constants/core-features.constant";
             <dgp-hamburger-menu dgp-hamburger-menu>
 
                 <dgp-hamburger-menu-header>
-                    DGP NG APP (v16)
+                    DGP NG APP (v16 mdc)
                 </dgp-hamburger-menu-header>
 
                 <dgp-hamburger-menu-entries>
