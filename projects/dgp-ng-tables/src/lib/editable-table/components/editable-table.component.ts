a4abff7150d1645e53fada6e61e7882dc44572ee
dgp-web <dgp-web@outlook.de>
2025-02-13 06:52:38 +0100

    Start adding info to table

--- projects/dgp-ng-tables/src/lib/editable-table/components/editable-table.component.ts
+++ projects/dgp-ng-tables/src/lib/editable-table/components/editable-table.component.ts
@@ -1,13 +1,50 @@
-import { Component, ChangeDetectionStrategy } from "@angular/core";
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { Many } from "data-modeling";
+
+export interface TableRow {
+    readonly tableRowKey: string;
+    readonly position: number;
+    readonly label: string;
+}
+
+export interface TableColumn {
+    readonly tableColumnKey: string;
+    readonly position: number;
+    readonly label: string;
+}
 
 @Component({
     selector: "dgp-editable-table",
     template: ``,
     styles: [`
 
-   `],
+    `],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DgpEditableTableComponent {
+
+    @Input()
+    rows: Many<TableRow>;
+
+    @Input()
+    columns: Many<TableColumn>;
+
+    @Input()
+    canAddRows: boolean;
+
+    @Input()
+    canRemoveRows: boolean;
+
+    @Input()
+    canMoveRows: boolean;
+
+    @Input()
+    canAddColumns: boolean;
+
+    @Input()
+    canRemoveColumns: boolean;
+
+    @Input()
+    canMoveColumns: boolean;
 
 }
