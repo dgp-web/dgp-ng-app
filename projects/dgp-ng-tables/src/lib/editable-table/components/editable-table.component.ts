077650ec8808fdaca4cdc0c8675a75759fe95c49
dgp-web <dgp-web@outlook.de>
2025-02-13 09:02:00 +0100

    Start adding format masks

--- projects/dgp-ng-tables/src/lib/editable-table/components/editable-table.component.ts
+++ projects/dgp-ng-tables/src/lib/editable-table/components/editable-table.component.ts
@@ -2,15 +2,105 @@ import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
 import { Many } from "data-modeling";
 
 export interface TableRow {
-    readonly tableRowKey: string;
+    readonly rowKey: string;
     readonly position: number;
     readonly label: string;
 }
 
+export enum FormatMaskType {
+    Auto = "auto",
+    Text = "text",
+    Integer = "integer",
+    Float = "float",
+    Boolean = "boolean",
+    Image = "image",
+    Date = "date"
+}
+
+export interface FormatMaskBase {
+    readonly type: FormatMaskType;
+}
+
+export interface TextFormatMask {
+    readonly type: FormatMaskType.Text;
+}
+
+export interface IntegerFormatMask {
+    readonly type: FormatMaskType.Integer;
+}
+
+export interface FloatFormatMask {
+    readonly type: FormatMaskType.Float;
+}
+
+export interface BooleanFormatMask {
+    readonly type: FormatMaskType.Boolean;
+}
+
+export interface ImageFormatMask {
+    readonly type: FormatMaskType.Image;
+}
+
+export interface DateFormatMask {
+    readonly type: FormatMaskType.Date;
+}
+
+export interface AutoFormatMask {
+    readonly type: FormatMaskType.Auto;
+}
+
+export type FormatMask = AutoFormatMask
+    | TextFormatMask
+    | IntegerFormatMask
+    | FloatFormatMask
+    | BooleanFormatMask
+    | ImageFormatMask
+    | DateFormatMask;
+
 export interface TableColumn {
-    readonly tableColumnKey: string;
+    readonly columnKey: string;
     readonly position: number;
     readonly label: string;
+    readonly formatMask: FormatMask;
+}
+
+export interface TableCell {
+    readonly rowKey: string;
+    readonly columnKey: string;
+    readonly formatMask: FormatMask;
+}
+
+export interface TableCellContent {
+    readonly rowKey: string;
+    readonly columnKey: string;
+    readonly value: any; // TODO
+}
+
+export interface TableDisplayConfig {
+    readonly sizingStrategy: "default" | "stretch";
+}
+
+export interface TableRowEditConfig {
+    readonly canAddRows: boolean;
+    readonly canRemoveRows: boolean;
+    readonly canMoveRows: boolean;
+    readonly canResizeRows: boolean;
+}
+
+export interface TableColumnEditConfig {
+    readonly canAddColumns: boolean;
+    readonly canRemoveColumns: boolean;
+    readonly canMoveColumns: boolean;
+    readonly canResizeColumns: boolean;
+    readonly canEditColumnFormatMasks: boolean;
+}
+
+export interface TableCellEditConfig {
+    readonly canEditCellFormatMasks: boolean;
+}
+
+export interface TableEditConfig extends TableRowEditConfig, TableColumnEditConfig, TableCellEditConfig {
+
 }
 
 @Component({
@@ -29,6 +119,10 @@ export class DgpEditableTableComponent {
     @Input()
     columns: Many<TableColumn>;
 
+    /**
+     * Row editing capabilities
+     */
+
     @Input()
     canAddRows: boolean;
 
@@ -39,6 +133,13 @@ export class DgpEditableTableComponent {
     canMoveRows: boolean;
 
     @Input()
+    canResizeRows: boolean;
+
+    /**
+     * Column editing capabilities
+     */
+
+    @Input()
     canAddColumns: boolean;
 
     @Input()
@@ -46,5 +147,11 @@ export class DgpEditableTableComponent {
 
     @Input()
     canMoveColumns: boolean;
+
+    @Input()
+    canResizeColumns: boolean;
+
+    @Input()
+    canEditColumnFormatMasks: boolean;
 
 }
