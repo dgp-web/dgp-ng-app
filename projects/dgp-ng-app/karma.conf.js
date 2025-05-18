040a449af96e238d18e1a447a3185926c989b7bf
dgp-web <dgp-web@outlook.de>
2025-05-18 15:52:09 +0200

    Adjust imports of polyfills in test.ts of dgp-ng-app and remove chrome-headless executable

--- projects/dgp-ng-app/karma.conf.js
+++ projects/dgp-ng-app/karma.conf.js
@@ -1,4 +1,3 @@
-process.env.CHROME_BIN = require("puppeteer").executablePath();
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
