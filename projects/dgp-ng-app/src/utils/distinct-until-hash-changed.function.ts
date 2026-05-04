b0976c0523b08e817d6a2aa7c10725a94605dce8
dgp-web <dgp-web@outlook.de>
2026-05-04 13:18:44 +0200

    Add distinctUntilDeepChanged with fast-deep-equal
    # Conflicts:
    #	package.json

--- projects/dgp-ng-app/src/utils/distinct-until-hash-changed.function.ts
+++ projects/dgp-ng-app/src/utils/distinct-until-hash-changed.function.ts
@@ -1,6 +1,16 @@
-import {distinctUntilChanged} from "rxjs/operators";
-import {getHashCode} from "./get-hash-code.function";
+import equal from 'fast-deep-equal';
+import { distinctUntilChanged } from "rxjs/operators";
+import { getHashCode } from "./get-hash-code.function";
 
 export function distinctUntilHashChanged<T>() {
     return distinctUntilChanged((x: T, y: T) => getHashCode(x) === getHashCode(y));
+}
+
+
+export function distinctUntilDeepChanged<T>() {
+  return distinctUntilChanged<T>((prev, curr) => {
+    if (prev === curr) return true;
+
+    return equal(prev, curr);
+  }); 
 }
