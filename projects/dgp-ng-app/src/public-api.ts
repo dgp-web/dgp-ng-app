1cb46c9a69a632c9219f50b2a96afe31cadbde8e
dgp-web <dgp-web@outlook.de>
2026-05-04 14:24:00 +0200

    Add missing explicit export

--- projects/dgp-ng-app/src/public-api.ts
+++ projects/dgp-ng-app/src/public-api.ts
@@ -252,7 +252,7 @@ export { DgpTileModule } from "./tile/tile.module";
 export { AppFeature } from "./utils/app-feature.model";
 export { createOnChangeEffect$ } from "./utils/create-on-change-effect$.function";
 export { defaultRuntimeChecks } from "./utils/default-runtime-checks";
-export { distinctUntilHashChanged } from "./utils/distinct-until-hash-changed.function";
+export { distinctUntilHashChanged, distinctUntilDeepChanged } from "./utils/distinct-until-hash-changed.function";
 export { DgpDisabledBase } from "./utils/dgp-disabled-base.directive";
 export { DgpEffectsBase } from "./utils/effects-base";
 export { filterEmpty } from "./utils/filter-empty.function";
