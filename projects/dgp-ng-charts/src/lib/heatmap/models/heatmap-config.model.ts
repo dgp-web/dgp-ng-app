89a807669da235da8a3b8b62246c08444dfb98f1
dgp-web <dgp-web@outlook.de>
2026-05-07 12:00:13 +0200

    Add max-tile extent to onfig

--- projects/dgp-ng-charts/src/lib/heatmap/models/heatmap-config.model.ts
+++ projects/dgp-ng-charts/src/lib/heatmap/models/heatmap-config.model.ts
@@ -1,9 +1,13 @@
 import { SharedChartConfig } from "../../shared/models";
-import { HeatmapDomainOverrides } from "./heatmap-domain-overrides.model";
 import { HeatmapDomainComputer } from "./heatmap-domain-computer.model";
+import { HeatmapDomainOverrides } from "./heatmap-domain-overrides.model";
 
 export interface HeatmapConfig extends SharedChartConfig {
     readonly domainComputer: HeatmapDomainComputer;
     readonly colorRange: ReadonlyArray<string>;
     readonly domainOverrides?: HeatmapDomainOverrides;
+    readonly maxTileExtent?: {
+        width: number;
+        height: number;
+    };
 }
