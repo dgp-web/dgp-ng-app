60be6dd6c787af05af55ec48f234accb0d5ba7a4
dgp-web <dgp-web@outlook.de>
2026-07-01 10:01:03 +0200

    Place ngOnDestroy at end of override in heatmap

--- projects/dgp-ng-charts/src/lib/heatmap/components/heatmap.component.ts
+++ projects/dgp-ng-charts/src/lib/heatmap/components/heatmap.component.ts
@@ -98,9 +98,8 @@ export class HeatmapComponent extends ChartComponentBase<ReadonlyArray<HeatmapTi
     }
 
     ngOnDestroy(): void {
-        super.ngOnDestroy();
-
         if(!this.redrawSubscription?.closed) this.redrawSubscription.unsubscribe();
+        super.ngOnDestroy();
     }
 
 
